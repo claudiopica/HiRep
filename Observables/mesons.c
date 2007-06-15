98fffe37c18d88b9b7401d1edad24bde2dd1f60e
Agostino Patella <agostino.patella@cern.ch>
2007-06-15 15:45:25 +0000

    Agostino - Aggiunto il correlatore g0_g0g5 per il calcolo della costante di decadimento del pione.
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/HiRep@19 37a69121-b030-0410-a54a-fdca83ffee47

--- Observables/mesons.c
+++ Observables/mesons.c
@@ -17,6 +17,12 @@
   +(_S3_)*_vector_prod_re_f(_spinor_c_(r,_C3_),(s).c3) \
   +(_S4_)*_vector_prod_re_f(_spinor_c_(r,_C4_),(s).c4))
 
+#define _spinor_perm_prod_im(r,s) \
+  ((_S1_)*_vector_prod_im_f(_spinor_c_(r,_C1_),(s).c1) \
+  +(_S2_)*_vector_prod_im_f(_spinor_c_(r,_C2_),(s).c2) \
+  +(_S3_)*_vector_prod_im_f(_spinor_c_(r,_C3_),(s).c3) \
+  +(_S4_)*_vector_prod_im_f(_spinor_c_(r,_C4_),(s).c4))
+
 
 #define MESON_DEFINITION \
 void NAME(float *out, suNf_spinor **qp) { \
@@ -39,6 +45,62 @@ void NAME(float *out, suNf_spinor **qp) { \
         s1 = &(qp[_INDEX_(i,4)][ipt[t][x][y][z]]); \
         s2 = &(qp[_INDEX_(i,_C4_)][ipt[t][x][y][z]]); \
         hc += (_S4_)*_spinor_perm_prod_re(*s1,*s2); \
+      } \
+    } \
+    out[t] = (_S0_)*hc; \
+  } \
+}
+
+
+#define MESON_DEFINITION_TWO_RE \
+void NAME(float *out, suNf_spinor **qp) { \
+  int t,x,y,z, i; \
+  suNf_spinor *s1; \
+  suNf_spinor *s2; \
+  for (t=0; t<T; ++t) { \
+    double hc=0.; \
+    for (x=0; x<L; ++x) for (y=0; y<L; ++y) for (z=0; z<L; ++z) { \
+      for (i=0; i<NF; ++i) { \
+        s1 = &(qp[_INDEX_(i,1)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D1_)][ipt[t][x][y][z]]); \
+        hc += (_T1_)*_spinor_perm_prod_re(*s1,*s2); \
+        s1 = &(qp[_INDEX_(i,2)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D2_)][ipt[t][x][y][z]]); \
+        hc += (_T2_)*_spinor_perm_prod_re(*s1,*s2); \
+        s1 = &(qp[_INDEX_(i,3)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D3_)][ipt[t][x][y][z]]); \
+        hc += (_T3_)*_spinor_perm_prod_re(*s1,*s2); \
+        s1 = &(qp[_INDEX_(i,4)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D4_)][ipt[t][x][y][z]]); \
+        hc += (_T4_)*_spinor_perm_prod_re(*s1,*s2); \
+      } \
+    } \
+    out[t] = (_S0_)*hc; \
+  } \
+}
+
+
+#define MESON_DEFINITION_TWO_IM \
+void NAME(float *out, suNf_spinor **qp) { \
+  int t,x,y,z, i; \
+  suNf_spinor *s1; \
+  suNf_spinor *s2; \
+  for (t=0; t<T; ++t) { \
+    double hc=0.; \
+    for (x=0; x<L; ++x) for (y=0; y<L; ++y) for (z=0; z<L; ++z) { \
+      for (i=0; i<NF; ++i) { \
+        s1 = &(qp[_INDEX_(i,1)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D1_)][ipt[t][x][y][z]]); \
+        hc += (_T1_)*_spinor_perm_prod_im(*s1,*s2); \
+        s1 = &(qp[_INDEX_(i,2)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D2_)][ipt[t][x][y][z]]); \
+        hc += (_T2_)*_spinor_perm_prod_im(*s1,*s2); \
+        s1 = &(qp[_INDEX_(i,3)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D3_)][ipt[t][x][y][z]]); \
+        hc += (_T3_)*_spinor_perm_prod_im(*s1,*s2); \
+        s1 = &(qp[_INDEX_(i,4)][ipt[t][x][y][z]]); \
+        s2 = &(qp[_INDEX_(i,_D4_)][ipt[t][x][y][z]]); \
+        hc += (_T4_)*_spinor_perm_prod_im(*s1,*s2); \
       } \
     } \
     out[t] = (_S0_)*hc; \
@@ -510,4 +572,49 @@ MESON_DEFINITION
 #undef _S0_
 
 
+
+#define NAME g5_g0g5_correlator_im
+
+#define _C1_ 1
+#define _C2_ 2
+#define _C3_ 3
+#define _C4_ 4
+
+#define _D1_ 3
+#define _D2_ 4
+#define _D3_ 1
+#define _D4_ 2
+
+#define _S1_ 1
+#define _S2_ 1
+#define _S3_ 1
+#define _S4_ 1
+
+#define _T1_ 1
+#define _T2_ 1
+#define _T3_ 1
+#define _T4_ 1
+
+#define _S0_ -1
+
+MESON_DEFINITION_TWO_IM
+
+#undef NAME
+#undef _C1_
+#undef _C2_
+#undef _C3_
+#undef _C4_
+#undef _D1_
+#undef _D2_
+#undef _D3_
+#undef _D4_
+#undef _S1_
+#undef _S2_
+#undef _S3_
+#undef _S4_
+#undef _T1_
+#undef _T2_
+#undef _T3_
+#undef _T4_
+#undef _S0_
 
