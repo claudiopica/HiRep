f61b78a696540c3aa94b59a0e124e8e1c7b22c80
Luigi Del Debbio <luigi.del.debbio@ed.ac.uk>
2007-08-11 14:14:36 +0000

    corrected headers for double precision code
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/HiRep@33 37a69121-b030-0410-a54a-fdca83ffee47

--- Include/dirac.h
+++ Include/dirac.h
@@ -12,17 +12,19 @@ void Dphi_(block_selector B, suNf_spinor *out, suNf_spinor *in);
 void Dphi(double m0, suNf_spinor *out, suNf_spinor *in);
 void g5Dphi(double m0, suNf_spinor *out, suNf_spinor *in);
 
-void Dphi_dble_(block_selector B, suNf_spinor_dble *out, suNf_spinor_dble *in);
-void Dphi_dble(double m0, suNf_spinor_dble *out, suNf_spinor_dble *in);
-void g5Dphi_dble(double m0, suNf_spinor_dble *out, suNf_spinor_dble *in);
+void Dphi_flt_(block_selector B, suNf_spinor_flt *out, suNf_spinor_flt *in);
+void Dphi_flt(double m0, suNf_spinor_flt *out, suNf_spinor_flt *in);
+void g5Dphi_flt(double m0, suNf_spinor_flt *out, suNf_spinor_flt *in);
 
 unsigned long int getMVM();
 
 /* p = out ; q = in */
+/*
 void Dphi_old(float m0, suNf_spinor *p, suNf_spinor *q);
 void g5Dphi_old(float m0, suNf_spinor *p, suNf_spinor *q);
 void Dphi_dble_old(double m0, suNf_spinor_dble *p, suNf_spinor_dble *q);
 void g5Dphi_dble_old(double m0, suNf_spinor_dble *p, suNf_spinor_dble *q);
+*/
 
 #endif
 	
