a5c6919529c31f0fd7d6d178f3e461a4403de7be
Antonio Rago <antonio.rago@plymouth.ac.uk>
2008-04-07 18:18:29 +0000

    In this file we define a geometry for a part of the total lattice that must be run  by a parallel(mpi) code.  In particular we define the geometry of the subset of the lattice that run on a  single node.  We have decided to include in this definition also a buffer that will contain  all the neighbours we need for a generic update.  This local lattice is then also divided in three mayor pieces:
     Inner lattice: the part of the local lattice that don't needs any information by other 
                    processors to be upgraded
     Border Lattice: the part of the local lattice that needs other information from
                     other processors
     Buffer lattice: is the buffer in which we will put the fields sended by other processor
      
     The sites in each different piece are organized in even-odd ordering.
    
                     
    
    
    
    
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-mpi@79 37a69121-b030-0410-a54a-fdca83ffee47

--- Geometry/geometry_init.c
+++ Geometry/geometry_init.c
@@ -55,7 +55,6 @@ void geometry_init() {
 		/* ipt_4d */
 		ALLOC(ipt_4d,VOLUME);
 
-#undef ALLOC
 
 		atexit(&free_memory);
 
@@ -68,7 +67,50 @@ void geometry_init() {
 
 
 
+void geometry_set()
+{
+  if (init) {
+    T=4;
+    X=4;
+    Y=4;
+    Z=4;
+    VOL3=X*Y*Z;
+    VOLUME=VOL3*T;
+  }	
+}
 
+void geometry_mem_alloc(geometry_descriptor * gd) {
+	if (init) {
+		int *cur;
+		size_t req_mem=0;
+		unsigned int VOL_SIZE=gd->gsize;
+		
+		req_mem+=2*4*VOL_SIZE; /* for iup and idn */
+		req_mem+=VOLUME;     /* for ipt */
+		
+		alloc_mem=malloc(req_mem*sizeof(int));
+		error((alloc_mem==NULL),1,"geometry_init [geometry_init.c]",
+		      "Cannot allocate memory");
+		
+		cur=alloc_mem;
+		
+		/* iup and idn */
+		ALLOC(iup,4*VOL_SIZE);
+		ALLOC(idn,4*VOL_SIZE);
+		/* ipt */
+		ALLOC(ipt,VOLUME);
+		/* ipt_4d */
+		
+
+		atexit(&free_memory);
+
+		init=0;
+	}
+}
+
+
+
+#undef ALLOC
 
 
 
--- Include/geometry.h
+++ Include/geometry.h
@@ -2,17 +2,33 @@
 #define GEOMETRY_H
 
 typedef struct {
-	int size;
-   int local_pieces; /* reticolo ristretto + output buffers */
-   int *start; /* starting index for each piece. len=tot_pieces */
-   int *end; /* ending index for each piece. len=tot_pieces */
-   int *len; /* length for each local piece for unique points. len=local_pieces */
+  unsigned int local_pieces; /* reticolo ristretto + output buffers */
+  unsigned int tot_pieces; /* above + input buffers */
+  unsigned int *start; /* starting index for each piece. len=tot_pieces */
+  unsigned int *len; /* length for each local piece for unique points. len=local_pieces */
+  unsigned int *tlen; /* total lenght for each piece. len=tot_pieces */
+  unsigned int ncopy; /* tot number of buffer copies needed to fill the holes. */
+  unsigned int *copy_from, *copy_to, *copy_len; /* len=ncopy */
+  int *sid; /* cartesian id of the node to which the local buffers must be sent. len=local_pieces*/
+  int *rid; /* cartesian id of the node from which the input buffers are received. len=tot_pieces-local_pieces*/
+  unsigned int gsize; /* global size of the associated array = \sum_i tlen_i */
+} geometry_descriptor;
+
+typedef struct {
+  int size;
+  int local_pieces; /* reticolo ristretto + output buffers */
+  int *start; /* starting index for each piece. len=tot_pieces */
+  int *end; /* ending index for each piece. len=tot_pieces */
+  int *len; /* length for each local piece for unique points. len=local_pieces */
 } spinor_descriptor;
 
 #define LOCAL_SD_FOR(sd,i,j) \
 	for(i=0;i<sd->local_pieces;i++) for(j=sd->start[i];j<sd->start[i]+sd->len[i];j++)
 
-void geometry_init();
+void geometry_mpi(void);
+void geometry_init(void);
+void geometry_set(void);
+void geometry_mem_alloc(geometry_descriptor*);
 void geometry_blocked(void);
 void geometry_blocked_noT(void);
 void geometry_lexi(void);
