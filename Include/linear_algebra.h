f893ee1183945b25fa3d53567f2b72f0f2a7ebe7
Claudio Pica <pica@cp3.sdu.dk>
2011-10-28 12:53:28 +0000

    Modified linear_algebra.[ch] to include GPU functions.
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-GPU@215 37a69121-b030-0410-a54a-fdca83ffee47

--- Include/linear_algebra.h
+++ Include/linear_algebra.h
@@ -18,15 +18,29 @@
 
 /* double precision */
 #define _SPINOR_FIELD_TYPE spinor_field
+#ifdef WITH_GPU
+#define _FUNC(a) a##_f_cpu
+#include "TMPL/linear_algebra.h.sdtmpl"
+#undef _FUNC
+#endif
+
 #define _FUNC(a) a##_f
 #include "TMPL/linear_algebra.h.sdtmpl"
+
 #undef _SPINOR_FIELD_TYPE
 #undef _FUNC
 
 /* single precision */
 #define _SPINOR_FIELD_TYPE spinor_field_flt
+#ifdef WITH_GPU
+#define _FUNC(a) a##_f_flt_cpu
+#include "TMPL/linear_algebra.h.sdtmpl"
+#undef _FUNC
+#endif
+
 #define _FUNC(a) a##_f_flt
 #include "TMPL/linear_algebra.h.sdtmpl"
+
 #undef _SPINOR_FIELD_TYPE
 #undef _FUNC
 
--- Inverters/linear_algebra.c
+++ Inverters/linear_algebra.c
@@ -16,19 +16,36 @@
  */
 
 
+
 /* double precision */
 
 #define _SPINOR_FIELD_TYPE spinor_field
 #define _SPINOR_TYPE suNf_spinor
-#define _FUNC(a) a##_f
 #define _REAL double
 #define _COMPLEX complex
 
+#ifdef WITH_GPU
+#define _FUNC(a) a##_f_cpu
 #include "TMPL/linear_algebra.c.sdtmpl"
 void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
 	_TWO_SPINORS_MATCHING(s1,s2);
 	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor));
 }
+#undef _FUNC
+#define _FUNC(a) a##_f
+#include "TMPL/linear_algebra_gpu.c.sdtmpl"
+void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
+	_TWO_SPINORS_MATCHING(s1,s2);
+	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor));
+}
+#else
+#define _FUNC(a) a##_f
+#include "TMPL/linear_algebra.c.sdtmpl"
+void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
+	_TWO_SPINORS_MATCHING(s1,s2);
+	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor));
+}
+#endif
 
 #undef _SPINOR_FIELD_TYPE
 #undef _SPINOR_TYPE
@@ -41,20 +58,35 @@ void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
 
 #define _SPINOR_FIELD_TYPE spinor_field_flt
 #define _SPINOR_TYPE suNf_spinor_flt
-#define _FUNC(a) a##_f_flt
 #define _REAL float
 #define _COMPLEX complex_flt
 
+#ifdef WITH_GPU
+#define _FUNC(a) a##_f_flt_cpu
 #include "TMPL/linear_algebra.c.sdtmpl"
 void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
 	_TWO_SPINORS_MATCHING(s1,s2);
-	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor_flt));
+	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor));
 }
+#undef _FUNC
+#define _FUNC(a) a##_f_flt
+#include "TMPL/linear_algebra_gpu.c.sdtmpl"
+void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
+	_TWO_SPINORS_MATCHING(s1,s2);
+	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor));
+}
+#else
+#define _FUNC(a) a##_f_flt
+#include "TMPL/linear_algebra.c.sdtmpl"
+void _FUNC(spinor_field_copy)(_SPINOR_FIELD_TYPE *s1, _SPINOR_FIELD_TYPE *s2) {
+	_TWO_SPINORS_MATCHING(s1,s2);
+	memcpy(s1->ptr,s2->ptr,s1->type->gsize*sizeof(suNf_spinor));
+}
+#endif
 
 #undef _SPINOR_FIELD_TYPE
 #undef _SPINOR_TYPE
 #undef _FUNC
 #undef _REAL
 #undef _COMPLEX
-
 
