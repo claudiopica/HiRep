3cdc446143f8ff4e97685c39d0101c85b490627b
Ari Hietanen <hietanen@cp3.sdu.dk>
2012-03-22 13:06:04 +0000

    Random momenta on GPU.
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-GPU@364 37a69121-b030-0410-a54a-fdca83ffee47

---
+++ Update/random_momenta_gpu.c
@@ -0,0 +1,17 @@
+/***************************************************************************\
+* Copyright (c) 2008, Claudio Pica                                          *   
+* All rights reserved.                                                      * 
+\***************************************************************************/
+
+#include "spinor_field.h"
+#include "suN_repr_func.h"
+#include "random.h"
+#include "update.h"
+#include "utils.h"
+#include <math.h>
+
+void gaussian_momenta(suNg_av_field *momenta) {
+  gaussian_momenta_cpu(momenta);
+  suNg_av_field_copy_to_gpu(momenta);
+}
+
