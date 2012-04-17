dea7440d98c4091722fafcf5efe3598b5b45c852
Claudio Pica <pica@cp3.sdu.dk>
2012-04-17 15:27:29 +0000

    Simplified single <-> double conversions. Needs testing.
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-GPU@410 37a69121-b030-0410-a54a-fdca83ffee47

--- Utils/single_double_utils.c
+++ Utils/single_double_utils.c
@@ -18,199 +18,105 @@
 #include "global.h"
 #include "spinor_field.h"
 
+
+static inline void assign_f2d(double *out, float *in, unsigned int len)
+{
+    while(len-->0) { out[len]=(double)in[len]; }
+}
+
+static inline void assign_d2f(float *out, double *in, unsigned int len)
+{
+    while(len-->0) { out[len]=(float)in[len]; }
+}
+
 void assign_u2ud_cpu(void)
 {
-  _DECLARE_INT_ITERATOR(ix);
-  int i,mu;
-  complex *r;
-  complex_flt *rf;
-
-  _MASTER_FOR(&glattice,ix){
-    for (mu=0;mu<4;mu++)
-    {
-      r=(complex*)(pu_gauge(ix,mu));
-      rf=(complex_flt*)(pu_gauge_flt(ix,mu));
-      
-#ifdef WITH_QUATERNIONS
-#if (NG != 2)
-#error "WITH_QUATERNIONS not defined with NG!=2"
-#else
-      r[0].re=(double)(rf[0].re);
-      r[0].im=(double)(rf[0].im);
-      r[1].re=(double)(rf[1].re);
-      r[1].im=(double)(rf[1].im);
-#endif
-#else
-      for (i=0;i<(NG*NG);++i)
-      {
-        r[i].re=(double)(rf[i].re);
-        r[i].im=(double)(rf[i].im);
-      }
-#endif
-      
-      project_to_suNg(pu_gauge(ix,mu));
+    for(int pidx=0; pidx<glattice.local_master_pieces;++pidx) {
+        int ix=glattice.master_start[pidx]; //first site in the block
+        double *r=(double*)(pu_gauge(ix,0));
+        float *rf=(float*)(pu_gauge_flt(ix,0));
+        
+        int len=glattice.master_end[pidx]-glattice.master_start[pidx]+1;
+        len*=4*sizeof(suNg)/sizeof(*r); //lenght of the block in real numbers
+        
+        assign_f2d(r, rf, len);
     }
-  }
 }
 
 
 void assign_ud2u_cpu(void)
 {
-  _DECLARE_INT_ITERATOR(ix);
-  int i,mu;
-  complex *r;
-  complex_flt *rf;
-
-  _MASTER_FOR(&glattice,ix){
-    for (mu=0;mu<4;mu++)
-    {
-      r=(complex*)(pu_gauge(ix,mu));
-      rf=(complex_flt*)(pu_gauge_flt(ix,mu));
-
-#ifdef WITH_QUATERNIONS
-#if (NG != 2)
-#error "WITH_QUATERNIONS not defined with NG!=2"
-#else
-      rf[0].re=(float)(r[0].re);
-      rf[0].im=(float)(r[0].im);
-      rf[1].re=(float)(r[1].re);
-      rf[1].im=(float)(r[1].im);
-#endif
-#else
-      for (i=0;i<(NG*NG);++i)
-      {
-        rf[i].re=(float)(r[i].re);
-        rf[i].im=(float)(r[i].im);
-      }
-#endif
-      
+    for(int pidx=0; pidx<glattice.local_master_pieces;++pidx) {
+        int ix=glattice.master_start[pidx]; //first site in the block
+        double *r=(double*)(pu_gauge(ix,0));
+        float *rf=(float*)(pu_gauge_flt(ix,0));
+        
+        int len=glattice.master_end[pidx]-glattice.master_start[pidx]+1;
+        len*=4*sizeof(suNg)/sizeof(*r); //lenght of the block in real numbers
+        
+        assign_d2f(rf, r, len);
     }
-  }
 }
 
 void assign_ud2u_f_cpu(void)
 {
-#ifdef WITH_QUATERNIONS
-  assign_ud2u_cpu();
-#else
-  _DECLARE_INT_ITERATOR(ix);
-  int i,mu;
-
-#ifdef REPR_ADJOINT
-  double *r;
-  float *rf;
-#else
-  complex *r;
-  complex_flt *rf;
-#endif
-
-  _MASTER_FOR(&glattice,ix){
-    for (mu=0;mu<4;mu++)
-    {
-#ifdef REPR_ADJOINT
-      r=(double*)(pu_gauge_f(ix,mu));
-      rf=(float*)(pu_gauge_f_flt(ix,mu));
-
-      for (i=0;i<(NF*NF);++i)
-        rf[i]=(float)(r[i]);
-#else
-      r=(complex*)(pu_gauge_f(ix,mu));
-      rf=(complex_flt*)(pu_gauge_f_flt(ix,mu));
-
-      for (i=0;i<(NF*NF);++i)
-      {
-        rf[i].re=(float)(r[i].re);
-        rf[i].im=(float)(r[i].im);
-      }
-#endif
+    for(int pidx=0; pidx<glattice.local_master_pieces;++pidx) {
+        int ix=glattice.master_start[pidx]; //first site in the block
+        double *r=(double*)(pu_gauge_f(ix,0));
+        float *rf=(float*)(pu_gauge_f_flt(ix,0));
+        
+        int len=glattice.master_end[pidx]-glattice.master_start[pidx]+1;
+        len*=4*sizeof(suNf)/sizeof(*r); //lenght of the block in real numbers
+        
+        assign_d2f(rf, r, len);
     }
-  }
-#endif //WITH_QUATERNIONS
 }
 
 void assign_u2ud_f_cpu(void)
 {
-#ifdef WITH_QUATERNIONS
-  assign_u2ud_cpu();
-#else
-  _DECLARE_INT_ITERATOR(ix);
-  int i,mu;
-#ifdef REPR_ADJOINT
-  double *r;
-  float *rf;
-#else
-  complex *r;
-  complex_flt *rf;
-#endif
-  
-  _MASTER_FOR(&glattice,ix){
-    for (mu=0;mu<4;mu++)
-    {
-#ifdef REPR_ADJOINT
-      r=(double*)(pu_gauge_f(ix,mu));
-      rf=(float*)(pu_gauge_f_flt(ix,mu));
-      
-      for (i=0;i<(NF*NF);++i)
-        r[i]=(double)(rf[i]);
-#else
-      r=(complex*)(pu_gauge_f(ix,mu));
-      rf=(complex_flt*)(pu_gauge_f_flt(ix,mu));
-      
-      for (i=0;i<(NF*NF);++i)
-      {
-        r[i].re=(double)(rf[i].re);
-        r[i].im=(double)(rf[i].im);
-      }
-#endif
+    for(int pidx=0; pidx<glattice.local_master_pieces;++pidx) {
+        int ix=glattice.master_start[pidx]; //first site in the block
+        double *r=(double*)(pu_gauge_f(ix,0));
+        float *rf=(float*)(pu_gauge_f_flt(ix,0));
+        
+        int len=glattice.master_end[pidx]-glattice.master_start[pidx]+1;
+        len*=4*sizeof(suNf)/sizeof(*r); //lenght of the block in real numbers
+        
+        assign_f2d(r, rf, len);
     }
-  }
-#endif //WITH_QUATERNIONS
 }
 
 
-void assign_s2sd_cpu(spinor_field *out, spinor_field_flt *in) {
-
-  int n;
-  float *i;
-  double *o;
-
-  _DECLARE_INT_ITERATOR(ix);
-  _DECLARE_SPINOR_ITERATOR(out);
-  _DECLARE_SPINOR_FLT_ITERATOR(in);
-
-  _TWO_SPINORS_MATCHING(in,out);
-
-  _TWO_SPINORS_FOR(out,in,ix) {
-    o=(double*)_SPINOR_PTR(out);
-    i=(float*)_SPINOR_PTR(in);
-    for (n=0;n<(8*NF);n++)
-    {
-      *(o++) = (double) *(i++);
+void assign_s2sd_cpu(spinor_field *out, spinor_field_flt *in) 
+{
+    _TWO_SPINORS_MATCHING(in,out);
+    
+    for(int pidx=0; pidx<in->type->local_master_pieces;++pidx) {
+        int ix=in->type->master_start[pidx]; //first site in the block
+        double *r=(double*)(_FIELD_AT(out,ix));
+        float *rf=(float*)(_FIELD_AT(in,ix));
+        
+        int len=in->type->master_end[pidx]-in->type->master_start[pidx]+1;
+        len*=sizeof(suNf_spinor)/sizeof(*r); //lenght of the block in real numbers
+        
+        assign_f2d(r, rf, len);
     }
-  }
-
 }
 
-void assign_sd2s_cpu(spinor_field_flt *out, spinor_field *in) {
-
-  int n;
-  float *o;
-  double *i;
-
-  _DECLARE_INT_ITERATOR(ix); 
-  _DECLARE_SPINOR_FLT_ITERATOR(out);
-  _DECLARE_SPINOR_ITERATOR(in);
-
-  _TWO_SPINORS_MATCHING(in,out);
-
-  _TWO_SPINORS_FOR(out,in,ix) {
-    o=(float*)_SPINOR_PTR(out);
-    i=(double*)_SPINOR_PTR(in);
-    for (n=0;n<(8*NF);n++)
-    {
-      *(o++) = (float) *(i++);
+void assign_sd2s_cpu(spinor_field_flt *out, spinor_field *in) 
+{
+    _TWO_SPINORS_MATCHING(in,out);
+    
+    for(int pidx=0; pidx<in->type->local_master_pieces;++pidx) {
+        int ix=in->type->master_start[pidx]; //first site in the block
+        double *r=(double*)(_FIELD_AT(in,ix));
+        float *rf=(float*)(_FIELD_AT(out,ix));
+        
+        int len=in->type->master_end[pidx]-in->type->master_start[pidx]+1;
+        len*=sizeof(suNf_spinor)/sizeof(*r); //lenght of the block in real numbers
+        
+        assign_d2f(rf, r, len);
     }
-  }
 }
 
 #ifndef WITH_GPU
--- Utils/single_double_utils_gpu.c
+++ Utils/single_double_utils_gpu.c
@@ -1,6 +1,6 @@
 /***************************************************************************\
-* Copyright (c) 20012, Ulrik Ishøj Søndergaard							*   
-* All rights reserved.											       * 
+* Copyright (c) 2012, Ulrik Ishøj Søndergaard, Claudio Pica                *   
+* All rights reserved.                                                     * 
 \***************************************************************************/
 
 /*******************************************************************************
@@ -11,6 +11,7 @@
 *
 *******************************************************************************/
 #ifdef WITH_GPU
+
 #include <stdlib.h>
 #include "utils.h"
 #include "suN.h"
@@ -19,131 +20,123 @@
 #include "spinor_field.h"
 #include "memory.h"
 
-/*
-void assign_s2sd(spinor_field *out, spinor_field_flt *in) {
-  spinor_field_copy_from_gpu_f_flt(in);
-  assign_s2sd_cpu(out,in);
-  spinor_field_copy_to_gpu_f(out);
-}
-
-void assign_sd2s(spinor_field_flt *out, spinor_field *in) {
-  spinor_field_copy_from_gpu_f(in);
-  assign_sd2s_cpu(out,in);
-  spinor_field_copy_to_gpu_f_flt(out);
-}
-*/
-
 
 #include "gpu.h"
  
  
- __global__ void assign_s2sd_kernel(suNf_spinor* out, suNf_spinor_flt* in, const int N)
+ __global__ void assign_f2d_kernel(double* out, float* in, const int N)
  {
- int ix = blockIdx.x*BLOCK_SIZE + threadIdx.x;
- ix = min(ix,N-1);						
- ((double*)(out))[ix]=(double)((float*)(in))[ix];
+     int ix = blockIdx.x*BLOCK_SIZE + threadIdx.x;
+     ix = min(ix,N-1);						
+     out[ix]=(double)in[ix];
  }
  
- __global__ void assign_sd2s_kernel(suNf_spinor_flt* out, suNf_spinor* in, const int N)
+ __global__ void assign_d2f_kernel(float* out, double* in, const int N)
  {
- int ix = blockIdx.x*BLOCK_SIZE + threadIdx.x;
- ix = min(ix,N-1);						
- ((float*)(out))[ix]=(float)((double*)(in))[ix];
+     int ix = blockIdx.x*BLOCK_SIZE + threadIdx.x;
+     ix = min(ix,N-1);						
+     out[ix]=(float)in[ix];
  }
-
-
-__global__ void assign_u2ud_quaternions_kernel(suNg* gauge, suNg_flt* gauge_flt, int N){ //Only for quaternions
-  suNg u;
-  suNg_flt u_flt;
-  int ix = blockIdx.x*BLOCK_SIZE+ threadIdx.x;
-  int i;
-  if (ix>=N/2) {
-    gauge+=2*N;
-    ix -= N/2;
-  }
-  for (i=0;i<4;++i){
-    _suNg_flt_read_gpu(N/2,u_flt,gauge_flt,ix,i);
-    u.c[0]=(double) u_flt.c[0];
-    u.c[1]=(double) u_flt.c[1];
-    u.c[2]=(double) u_flt.c[2];
-    u.c[3]=(double) u_flt.c[3];
-    _suNg_write_gpu(N/2,u,gauge,ix,i);
-  }
-}
-
-__global__ void assign_ud2u_quaternions_kernel(suNg* gauge, suNg_flt* gauge_flt, int N){ //Only for quaternions
-  suNg u;
-  suNg_flt u_flt;
-  int ix = blockIdx.x*BLOCK_SIZE+ threadIdx.x;
-  int i;
-  if (ix>=N/2) {
-    gauge+=2*N;
-    gauge_flt+=2*N;
-    ix -= N/2;
-  }
-  for (i=0;i<4;++i){
-    _suNg_read_gpu(N/2,u,gauge,ix,i);
-    u_flt.c[0]=(float) u.c[0];
-    u_flt.c[1]=(float) u.c[1];
-    u_flt.c[2]=(float) u.c[2];
-    u_flt.c[3]=(float) u.c[3];
-    _suNg_flt_write_gpu(N/2,u_flt,gauge_flt,ix,i);
-  }
-}
 
 
 void assign_s2sd(spinor_field *out, spinor_field_flt *in) {
-  int size, grid;
-  size = in->type->master_end[0] -  in->type->master_start[0] + 1;
-  size *= sizeof(suNf_spinor)/sizeof(double);				
-  grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 	// NF is defined in suN_types.h
-  assign_s2sd_kernel<<<grid,BLOCK_SIZE>>>(START_SP_ADDRESS_GPU(out),START_SP_ADDRESS_GPU(in), size);
-  CudaCheckError();	
+    int ix, size, grid;
+    double *r;
+    float *rf;
+    
+    _TWO_SPINORS_MATCHING(in,out);
+    
+    ix=in->type->master_start[0]; //first site in the block
+    r=(double*)(_FIELD_AT(out,ix));
+    rf=(float*)(_FIELD_AT(in,ix));
+    size = in->type->master_end[0] -  in->type->master_start[0] + 1;
+    size *= sizeof(suNf_spinor)/sizeof(double); ////lenght of the block in real numbers	
+    
+    grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 
+    assign_f2d_kernel<<<grid,BLOCK_SIZE>>>(r, rf, size);
+    CudaCheckError();	
 }
 
 void assign_sd2s(spinor_field_flt *out, spinor_field *in) {
-  int size, grid;
-  size = in->type->master_end[0] -  in->type->master_start[0] + 1;
-  size *= sizeof(suNf_spinor)/sizeof(double);				
-  grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 	// NF is defined in suN_types.h
-  assign_sd2s_kernel<<<grid,BLOCK_SIZE>>>(START_SP_ADDRESS_GPU(out),START_SP_ADDRESS_GPU(in), size);
-  CudaCheckError();
+    int ix, size, grid;
+    double *r;
+    float *rf;
+    
+    _TWO_SPINORS_MATCHING(in,out);
+    
+    ix=in->type->master_start[0]; //first site in the block
+    r=(double*)(_FIELD_AT(in,ix));
+    rf=(float*)(_FIELD_AT(out,ix));
+    size = in->type->master_end[0] -  in->type->master_start[0] + 1;
+    size *= sizeof(suNf_spinor)/sizeof(double); ////lenght of the block in real numbers	
+    
+    grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 
+    assign_d2f_kernel<<<grid,BLOCK_SIZE>>>(rf, r, size);
+    CudaCheckError();	
 }
 
 void assign_u2ud(){
-  int N = u_gauge->type->master_end[0] -  u_gauge->type->master_start[0] + 1;
-  int grid = N/BLOCK_SIZE + ((N % BLOCK_SIZE == 0) ? 0 : 1);
-#ifdef WITH_QUATERNIONS
-  assign_u2ud_quaternions_kernel<<<grid,BLOCK_SIZE>>>(u_gauge->gpu_ptr,u_gauge_flt->gpu_ptr,N);
-#else
-#error : single <-> double : GPU only with quaternions
-#endif
+    int ix, size, grid;
+    double *r;
+    float *rf;
+    
+    ix=glattice.master_start[0]; //first site in the block
+    r=(double*)(pu_gauge(ix,0));
+    rf=(float*)(pu_gauge_flt(ix,0));
+    size=glattice.master_end[0] - glattice.master_start[0] + 1;
+    size*=4*sizeof(suNg)/sizeof(double); //lenght of the block in real numbers
+    
+    grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 
+    assign_f2d_kernel<<<grid,BLOCK_SIZE>>>(r, rf, size);
+    CudaCheckError();	
 }
 
 void assign_ud2u(){
-  int N = u_gauge->type->master_end[0] -  u_gauge->type->master_start[0] + 1;
-  int grid = N/BLOCK_SIZE + ((N % BLOCK_SIZE == 0) ? 0 : 1);
-#ifdef WITH_QUATERNIONS
-  assign_ud2u_quaternions_kernel<<<grid,BLOCK_SIZE>>>(u_gauge->gpu_ptr,u_gauge_flt->gpu_ptr,N);
-#else
-#error : single <-> double : GPU only with quaternions
-#endif
+    int ix, size, grid;
+    double *r;
+    float *rf;
+    
+    ix=glattice.master_start[0]; //first site in the block
+    r=(double*)(pu_gauge(ix,0));
+    rf=(float*)(pu_gauge_flt(ix,0));
+    size=glattice.master_end[0] - glattice.master_start[0] + 1;
+    size*=4*sizeof(suNg)/sizeof(double); //lenght of the block in real numbers
+    
+    grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 
+    assign_d2f_kernel<<<grid,BLOCK_SIZE>>>(rf, r, size);
+    CudaCheckError();	
 }
 
 void assign_u2ud_f(){
-#ifdef WITH_QUATERNIONS
-  assign_u2ud();
-#else
-#error : single <-> double : GPU only with quaternions
-#endif 
+    int ix, size, grid;
+    double *r;
+    float *rf;
+    
+    ix=glattice.master_start[0]; //first site in the block
+    r=(double*)(pu_gauge_f(ix,0));
+    rf=(float*)(pu_gauge_f_flt(ix,0));
+    size=glattice.master_end[0] - glattice.master_start[0] + 1;
+    size*=4*sizeof(suNf)/sizeof(double); //lenght of the block in real numbers
+    
+    grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 
+    assign_f2d_kernel<<<grid,BLOCK_SIZE>>>(r, rf, size);
+    CudaCheckError();	
 }
 
 void assign_ud2u_f(){
-#ifdef WITH_QUATERNIONS
-  assign_ud2u();
-#else
-#error : single <-> double : GPU only with quaternions
-#endif 
+    int ix, size, grid;
+    double *r;
+    float *rf;
+    
+    ix=glattice.master_start[0]; //first site in the block
+    r=(double*)(pu_gauge_f(ix,0));
+    rf=(float*)(pu_gauge_f_flt(ix,0));
+    size=glattice.master_end[0] - glattice.master_start[0] + 1;
+    size*=4*sizeof(suNf)/sizeof(double); //lenght of the block in real numbers
+    
+    grid = size/BLOCK_SIZE + ((size % BLOCK_SIZE == 0) ? 0 : 1); 
+    assign_d2f_kernel<<<grid,BLOCK_SIZE>>>(rf, r, size);
+    CudaCheckError();	
 }
 
 
