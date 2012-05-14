2060c6f2efda433e2d2aa48e8327bf28421facf3
Ulrik Ishøj Søndergaard <uis@cp3.sdu.dk>
2012-05-14 10:35:25 +0000

    group verification now works for SO(N).
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-SO4@420 37a69121-b030-0410-a54a-fdca83ffee47

--- StaticPotential/verify_group.c
+++ StaticPotential/verify_group.c
@@ -32,26 +32,6 @@
 
 
 
-#define _suNg_im_sq(k,u) \
-(k)=\
-+_complex_im((u).c[0])*_complex_im((u).c[0])\
-+_complex_im((u).c[1])*_complex_im((u).c[1])\
-+_complex_im((u).c[2])*_complex_im((u).c[2])\
-+_complex_im((u).c[3])*_complex_im((u).c[3])\
-+_complex_im((u).c[4])*_complex_im((u).c[4])\
-+_complex_im((u).c[5])*_complex_im((u).c[5])\
-+_complex_im((u).c[6])*_complex_im((u).c[6])\
-+_complex_im((u).c[7])*_complex_im((u).c[7])\
-+_complex_im((u).c[8])*_complex_im((u).c[8])\
-+_complex_im((u).c[9])*_complex_im((u).c[9])\
-+_complex_im((u).c[10])*_complex_im((u).c[10])\
-+_complex_im((u).c[11])*_complex_im((u).c[11])\
-+_complex_im((u).c[12])*_complex_im((u).c[12])\
-+_complex_im((u).c[13])*_complex_im((u).c[13])\
-+_complex_im((u).c[14])*_complex_im((u).c[14])\
-+_complex_im((u).c[15])*_complex_im((u).c[15])
-
-
 
 char cnfg_filename[256]="";
 char list_filename[256]="";
@@ -71,40 +51,13 @@ typedef struct {
 
 
 
-/*static void polyako_1P(complex* poly, int tx[3], int mu) {
-	int i, x[4];
-	int glb[4]={GLB_T,GLB_X,GLB_Y,GLB_Z};
-	suNg omega;
-	suNg omega2;
-	
-	error(mu<0 || mu>3,1,"polyako_1P [polyak]","Bad value for direction parameter.");
-	
-	x[(mu+1)%4]=tx[0];
-	x[(mu+2)%4]=tx[1];
-	x[(mu+3)%4]=tx[2];
-	
-	_suNg_unit(omega);
-	for(x[mu]=0; x[mu]<glb[mu]; ) {
-		i=ipt(x[0],x[1],x[2],x[3]);
-		_suNg_times_suNg(omega2,omega,*pu_gauge(i,mu));
-		x[mu]++;
-		i=ipt(x[0],x[1],x[2],x[3]);
-		_suNg_times_suNg(omega,omega2,*pu_gauge(i,mu));
-		x[mu]++;
-	}
-	
-	_suNg_trace_re(poly->re,omega);
-	_suNg_trace_im(poly->im,omega);
-} */
+
 
 void identity_measure() {
 
 	int tx[4];
 	int glb[4]={GLB_T,GLB_X,GLB_Y,GLB_Z};
-//	complex cnum;
 	double rnum=0;
-//	double inum=0;
-//	cnum.re=cnum.im=0.;
 	suNg Uf;
 	int i;
 	
@@ -119,9 +72,7 @@ void identity_measure() {
 					_suNg_times_suNg_dagger(Uf,*pu_gauge(i,mu),*pu_gauge(i,mu));
 					_suNg_sqnorm(tmp,Uf);
 					//  Take the norm
-					rnum += (tmp>4) ? tmp-4 : 4- tmp ;
-//					_suNg_im_sq(tmp,*pu_gauge(i,mu));
-//					inum +=tmp;
+					rnum += (tmp>NG) ? tmp-NG : NG- tmp ;
 				}}}}}
 	//_complex_1(cnum);
 
