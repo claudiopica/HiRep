59bc026bb8a2a81f522a5cb055a8e0384aa9798a
Agostino Patella <agostino.patella@cern.ch>
2012-08-22 14:03:49 +0000

    using the macro AMALLOC_MEASURE, amalloc/afree will keep trace of the amount of memory allocated
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-HMC@456 37a69121-b030-0410-a54a-fdca83ffee47

--- Memory/amalloc.c
+++ Memory/amalloc.c
@@ -14,6 +14,75 @@
 #include <stdlib.h>
 #include "memory.h"
 
+
+#ifdef AMALLOC_MEASURE
+
+#include <string.h>
+#include "logger.h"
+
+typedef struct {
+  void* addr;
+  size_t size;
+} mem_map_t;
+
+static mem_map_t *allocated = NULL;
+static size_t nallocated = 0;
+static double memory_Mb = 0.;
+static double max_memory_Mb = 0.;
+
+void insert(void *addr, size_t size) {
+  mem_map_t *tmp;
+  tmp = malloc(sizeof(mem_map_t)*(nallocated+1));
+  
+  if(nallocated>0) memcpy(tmp,allocated,sizeof(mem_map_t)*nallocated);
+  tmp[nallocated].addr=addr;
+  tmp[nallocated].size=size;
+  nallocated++;
+  
+  if(nallocated>0) free(allocated);
+  allocated=tmp;
+  
+  memory_Mb += size/1048576.;
+  
+  if(memory_Mb>max_memory_Mb) {
+    max_memory_Mb=memory_Mb;
+    lprintf("AMALLOC",0,"Total memory allocated with amalloc: %.6f Mb\n", max_memory_Mb);
+  }
+
+  /*lprintf("AMALLOC",0,"Total memory allocated with amalloc: %.6f Mb\n", memory_Mb);*/
+}
+
+void remove(void *addr) {
+  error(nallocated==0,1,"amalloc.c","[remove] nallocated==0!");
+  int found=(1==0);
+  size_t at;
+  mem_map_t *tmp;
+  
+  for(at=0;at<nallocated;at++)
+  if(allocated[at].addr==addr) {
+    found=(1==1);
+    break;
+  }
+  
+  error(!found,1,"amalloc.c","[remove] !found");
+
+  memory_Mb -= allocated[at].size/1048576.;
+  
+  tmp=NULL;
+  if(nallocated!=1) tmp = malloc(sizeof(mem_map_t)*(nallocated-1));
+  if(at!=0) memcpy(tmp,allocated,sizeof(mem_map_t)*at);
+  if(nallocated-at-1!=0) memcpy(tmp+at,allocated+at+1,sizeof(mem_map_t)*(nallocated-at-1));
+  nallocated--;
+  free(allocated);
+  allocated=tmp;
+
+  /*lprintf("AMALLOC",0,"Total memory allocated with amalloc: %.6f Mb\n", memory_Mb);*/
+}
+
+#endif
+
+
+
 struct addr_t
 {
    char *addr;
@@ -51,7 +120,11 @@ void *amalloc(size_t size,int p)
    (*new).true_addr=true_addr;
    (*new).next=first;
    first=new;
-                   
+
+#ifdef AMALLOC_MEASURE
+   insert((void*)addr,size);
+#endif
+
    return((void*)(addr));
 }
 
@@ -59,6 +132,10 @@ void *amalloc(size_t size,int p)
 void afree(void *addr)
 {
    struct addr_t *p,*q;
+
+#ifdef AMALLOC_MEASURE
+   remove(addr);
+#endif
 
    q=NULL;
    
