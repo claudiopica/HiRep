18a275fa0df19ebce0889101b57d25e282ee0e4c
Agostino Patella <agostino.patella@cern.ch>
2012-08-22 08:22:43 +0000

    finalize_process added in error
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-HMC@452 37a69121-b030-0410-a54a-fdca83ffee47

--- Error/error.c
+++ Error/error.c
@@ -22,6 +22,7 @@ void error(int test,int no,char *name,char *text)
    {
       lprintf("ERROR",0,"%s:\n%s\n",name,text);
       lprintf("ERROR",0,"Exiting program...\n");
+      finalize_process();
       exit(no);
    }
 }
