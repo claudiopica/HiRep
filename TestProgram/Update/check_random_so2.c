8e82000b7f20f5c0e011c53611dc19014176547a
Ulrik Ishøj Søndergaard <uis@cp3.sdu.dk>
2013-01-15 08:47:53 +0000

    Adding test file for random SO(2) angles
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/Branches/HiRep-SO4@500 37a69121-b030-0410-a54a-fdca83ffee47

---
+++ TestProgram/Update/check_random_so2.c
@@ -0,0 +1,28 @@
+/******************************************************************************* 
+* Check that the molecular dynamics evolution is reversible
+* 
+*
+*******************************************************************************/
+
+#define MAIN_PROGRAM
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "update.h"
+#include "global.h"
+#include "logger.h"
+
+int main(int argc,char *argv[])
+{
+
+
+for (int i=0;i<100000;i++){
+   lprintf("THETA",0," %f \n", random_so2(2));
+}
+
+
+   lprintf("THETA",0," \n");
+   exit(0);
+}
