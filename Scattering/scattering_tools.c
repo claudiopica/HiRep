dbd731c4577da7d24b3a39bfe5a1e208c618accd
Vincent Drach <vincent.drach@plymouth.ac.uk>
2014-05-13 10:07:24 +0000

    Scattering : Fixed few bugs in the routine without spin dilution
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/HiRep@620 37a69121-b030-0410-a54a-fdca83ffee47

--- Scattering/scattering_tools.c
+++ Scattering/scattering_tools.c
@@ -73,44 +73,30 @@ static void fix_T_bc(int tau){
 
 
 void measure_pion_scattering(double* m, int nhits,int conf_num, double precision,int ts){
+	int k;
+
 	spinor_field* source_ts = alloc_spinor_field_f(4,&glattice);
 	spinor_field* source_tsp1 = alloc_spinor_field_f(4,&glattice);
-
 	spinor_field* prop_ts =  alloc_spinor_field_f(4 ,&glattice);
 	spinor_field* prop_tsp1 =  alloc_spinor_field_f(4 ,&glattice);
 
-	int k;
 	struct timeval start, end, etime;
 
 	gettimeofday(&start,0);
 	init_propagator_eo(1, m, precision);
 
 	for (k=0;k<nhits;k++){
-		lprintf("MAIN",0,"In the nhits loops ! \n");
 		// to have dirichlet boundary conditions for testing only :
 		//		fix_T_bc(ts);
 
-	
+		// TO DO : randomize ts
+
 		spinor_field_zero_f(source_ts);
 		create_diluted_source_equal_atau(source_ts, ts);
 		calc_propagator(prop_ts,source_ts,4);
 
-		//Print_spinor_field(&source_ts[0],0,0,0,0);				
-		//Print_spinor_field(&source_ts[1],0,0,0,0);				
-		//Print_spinor_field(&source_ts[0],0,0,0,1);				
-		//Print_spinor_field(&source_ts[0],1,0,0,0);				
-		//Print_spinor_field(&source_ts[1],1,0,0,0);				
-
-
 		spinor_field_zero_f(source_tsp1);
 		create_diluted_source_equal_atau(source_tsp1, ts+1);
-
-		//Print_spinor_field(&source_tsp1[0],0,0,0,1);				
-		//Print_spinor_field(&source_tsp1[1],0,0,0,1);				
-		//Print_spinor_field(&source_tsp1[0],0,0,0,1);				
-		//Print_spinor_field(&source_tsp1[0],1,0,0,0);				
-		//Print_spinor_field(&source_tsp1[1],1,0,0,0);				
-
 		calc_propagator(prop_tsp1,source_tsp1,4);
 
 		// for checks
@@ -122,7 +108,9 @@ void measure_pion_scattering(double* m, int nhits,int conf_num, double precision
 		lprintf("MAIN",0,"Start to perform the contractions ... \n");
 		contract_pion_scatt(prop_ts,prop_tsp1,k,ts);
 		lprintf("MAIN",0,"Contraction done\n");
-	}
+
+	}  // end loop nhits
+
 	free_propagator_eo();
 
 	free_spinor_field_f(source_ts);
@@ -132,31 +120,36 @@ void measure_pion_scattering(double* m, int nhits,int conf_num, double precision
 
 	gettimeofday(&end,0);
 	timeval_subtract(&etime,&end,&start);
-	lprintf("TIMING",0,"Sources generation, invert and contract for %i sources done [%ld sec %ld usec]\n",nhits,etime.tv_sec,etime.tv_usec);
+	lprintf("TIMING",0,"Sources generation, invert and contract for %i sources with spin dilution done [%ld sec %ld usec]\n",nhits,etime.tv_sec,etime.tv_usec);
 
-	// debugging or should I code it properly for future project ?
+	// run without spin dilution 
 
 	spinor_field* source2 = alloc_spinor_field_f(1,&glattice);
 	spinor_field* prop_ts_1 =  alloc_spinor_field_f(1 ,&glattice);
 	spinor_field* prop_tsp1_1 =  alloc_spinor_field_f(1 ,&glattice);
 	init_propagator_eo(1, m, precision);
 
-	create_diluted_source_equal_spinorfield1(source2,ts);
+	gettimeofday(&start,0);
+	for (k=0;k<nhits;k++){
 
-	calc_propagator(prop_ts_1,source2,1);// No dilution
+		spinor_field_zero_f(source2);
+		create_diluted_source_equal_spinorfield1(source2,ts);
+		calc_propagator(prop_ts_1,source2,1);// No spin dilution
 
-	spinor_field_zero_f(source2);
+		spinor_field_zero_f(source2);
+		create_diluted_source_equal_spinorfield1(source2,ts+1);
+		calc_propagator(prop_tsp1_1,source2,1);// No spin dilution
 
-	create_diluted_source_equal_spinorfield1(source2,ts+1);
-
-	calc_propagator(prop_tsp1_1,source2,1);// No dilution
-
-//	contract_pion_scatt_1spinorfield(prop_ts_1,prop_tsp1_1,0,ts);
-
+		contract_pion_scatt_1spinorfield(prop_ts_1,prop_tsp1_1,k,ts);
+	}
 	free_propagator_eo();
 	free_spinor_field_f(source2);
 	free_spinor_field_f(prop_tsp1_1);
 	free_spinor_field_f(prop_ts_1);
+
+	gettimeofday(&end,0);
+	timeval_subtract(&etime,&end,&start);
+	lprintf("TIMING",0,"Sources generation, invert and contract for %i sources without spin dilution done [%ld sec %ld usec]\n",nhits,etime.tv_sec,etime.tv_usec);
 
 
 }
@@ -228,9 +221,9 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 		for (beta=0;beta<4;beta++) for (gamma=0;gamma<4;gamma++){
 			_complex_0(B1[beta][gamma][t]);
 			_complex_0(B2[beta][gamma][t]);
- 			_complex_0(C1[beta][gamma][t]);
+			_complex_0(C1[beta][gamma][t]);
 			_complex_0(C2[beta][gamma][t]);
- 		}
+		}
 
 
 
@@ -240,9 +233,6 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 
 	for (t=0; t<T; t++)
 	{
-
-		//		if(COORD[0]==((t+ts)%GLB_T)/T) {// Check that tau is in this thread.	
-		/* what is tc and offset ? MPI related ? */
 		tc = (zerocoord[0]+t+GLB_T-ts)%GLB_T;
 
 		/* loop on spatial volume */
@@ -250,10 +240,6 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 
 			ix_pts = ipt(t,x,y,z);
 			ix_pts_pa = iup(ix_pts,0);
-
-			//			ix_pts_pa = ipt((t+ts+1)%GLB_T,x,y,z);
-			//			ix_pts = ipt((t+ts)%GLB_T,x,y,z);
-			//			printf("%i %i \n",t+ts+1,(t+ts+1)%GLB_T);
 
 			// A term
 			for (beta=0;beta<4;beta++)
@@ -265,20 +251,31 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 			_spinmatrix_mul_trace_assign(A1[tc], sm1, sm1);
 			_spinmatrix_mul_trace_assign(A2[tc], sm2, sm2);
 
-			//			if (t==0)  printf("A1.re %3.10e A2.re  %3.10e A1.im %3.10e A2.im  %3.10e\n",A1.re,A2.re,A1.im,A2.im);
 
 			// B term
 
 
+			//	for (beta=0;beta<4;beta++)
+			//			{
+			//				for (mu=0;mu<4;mu++)
+			//				{
+			//					_spinmatrix_assign_row(sm1, *_FIELD_AT(&phi_ts[mu],ix_pts_pa), beta); //  mu : source,  beta : internal one. A_mu beta
+			//					_spinmatrix_assign_row(sm2, *_FIELD_AT(&phi_tsp1[mu],ix_pts_pa), beta); // B_mu beta
+			//
+			//					_spinmatrix_assign_row(sm3, *_FIELD_AT(&phi_tsp1[mu],ix_pts), beta);
+			//					_spinmatrix_assign_row(sm4, *_FIELD_AT(&phi_ts[mu],ix_pts), beta);
+			//				}
+			//			}
 
-				for (mu=0;mu<4;mu++)
-				{
-					_spinmatrix_assign_row(sm1, *_FIELD_AT(&phi_ts[mu],ix_pts_pa), mu); //  mu : source,  beta : internal one. A_mu beta
-					_spinmatrix_assign_row(sm2, *_FIELD_AT(&phi_tsp1[mu],ix_pts_pa), mu); // B_mu beta
 
-					_spinmatrix_assign_row(sm3, *_FIELD_AT(&phi_tsp1[mu],ix_pts), mu);
-					_spinmatrix_assign_row(sm4, *_FIELD_AT(&phi_ts[mu],ix_pts), mu);
-				}
+			for (mu=0;mu<4;mu++)
+			{
+				_spinmatrix_assign_row(sm1, *_FIELD_AT(&phi_ts[mu],ix_pts_pa), mu); //  mu : source,  beta : internal one. A_mu beta
+				_spinmatrix_assign_row(sm2, *_FIELD_AT(&phi_tsp1[mu],ix_pts_pa), mu); // B_mu beta
+
+				_spinmatrix_assign_row(sm3, *_FIELD_AT(&phi_tsp1[mu],ix_pts), mu);
+				_spinmatrix_assign_row(sm4, *_FIELD_AT(&phi_ts[mu],ix_pts), mu);
+			}
 
 			// compute a new spinmatrix  : (A_mu beta * B_gamma_beta )
 			// trace color index -> it becomes a non standard struct. with only spin indices
@@ -288,31 +285,22 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 			{
 				for (gamma=0;gamma<4;gamma++)
 				{
-          _spinor_prod_assign_f(B1[mu][gamma][tc],sm1.c[mu],sm2.c[gamma]);
-          _spinor_prod_assign_f(B2[mu][gamma][tc],sm3.c[mu],sm4.c[gamma]);
+
+					_spinor_prod_assign_f(B1[mu][gamma][tc],sm1.c[mu],sm2.c[gamma]);
+					_spinor_prod_assign_f(B2[mu][gamma][tc],sm3.c[mu],sm4.c[gamma]);
 				}
 			}
 
 			// C term   : useless C^ast = B
-			for (beta=0;beta<4;beta++)
-			{
-				_spinmatrix_assign_row(sm1, *_FIELD_AT(&phi_tsp1[beta],ix_pts_pa), beta);
-				_spinmatrix_assign_row(sm2, *_FIELD_AT(&phi_ts[beta],ix_pts_pa), beta);
 
-				_spinmatrix_assign_row(sm3, *_FIELD_AT(&phi_ts[beta],ix_pts), beta);
-				_spinmatrix_assign_row(sm4, *_FIELD_AT(&phi_tsp1[beta],ix_pts), beta);
-
-			}
-
-      for (mu=0;mu<4;mu++)
+			for (mu=0;mu<4;mu++)
 			{
 				for (gamma=0;gamma<4;gamma++)
 				{
-          _spinor_prod_assign_f(C1[mu][gamma][tc],sm1.c[mu],sm2.c[gamma]);
-          _spinor_prod_assign_f(C2[mu][gamma][tc],sm3.c[mu],sm4.c[gamma]);
+					_spinor_prod_assign_f(C1[mu][gamma][tc],sm2.c[mu],sm1.c[gamma]);
+					_spinor_prod_assign_f(C2[mu][gamma][tc],sm4.c[mu],sm3.c[gamma]);
 				}
 			}
-
 
 			// D term
 			for (beta=0;beta<4;beta++)
@@ -332,7 +320,6 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 
 		corr_tmp[0][tc] = A1[tc].re;
 		corr_tmp[1][tc] = A2[tc].re;
-		//	}
 
 	} /* end loop on time */
 
@@ -372,7 +359,6 @@ void contract_pion_scatt(spinor_field* phi_ts,spinor_field* phi_tsp1,int k,int t
 	for (t=0;t<T;t++){
 
 		tc = (zerocoord[0]+t+GLB_T-ts)%GLB_T;
-		//		if(COORD[0]==((t+ts)%GLB_T)/T) {// Check that tau is in this thread.	
 		//		corr_2pt[tc] = 0.5 *(corr_tmp[0][(tc+GLB_T)%GLB_T]+corr_tmp[1][tc])/(GLB_VOL3);
 		corr_2pt[tc] =  (corr_tmp[1][tc])/(GLB_VOL3);
 		//		}
@@ -398,8 +384,8 @@ void contract_pion_scatt_1spinorfield(spinor_field* phi_ts,spinor_field* phi_tsp
 	int x,y,z,t,tc;
 	int ix,ix_pts_pa,ix_pts;
 	int beta,mu,gamma,l,a;
-	complex A1,A2,B1,B2,tmpc;
-	complex C1,C2,D1,D2;
+	complex *A1,*A2,*B1,*B2,tmpc;
+	complex *C1,*C2,*D1,*D2;
 
 	double norm;
 	struct timeval start, end, etime;
@@ -425,6 +411,15 @@ void contract_pion_scatt_1spinorfield(spinor_field* phi_ts,spinor_field* phi_tsp
 		tmp2[l]=(complex*) malloc(sizeof(complex)*4);
 
 	}
+	A1 =  malloc(sizeof(complex)*GLB_T);
+	A2 =  malloc(sizeof(complex)*GLB_T);
+	B1 =  malloc(sizeof(complex)*GLB_T);
+	B2 =  malloc(sizeof(complex)*GLB_T);
+	C1 =  malloc(sizeof(complex)*GLB_T);
+	C2 =  malloc(sizeof(complex)*GLB_T);
+	D1 =  malloc(sizeof(complex)*GLB_T);
+	D2 =  malloc(sizeof(complex)*GLB_T);
+
 
 	for (l=0;l<2 ;++l)  corr_tmp[l]=(double*) malloc(sizeof(double)*GLB_T);
 	corr_2pt=(double*) malloc(sizeof(double)*GLB_T);
@@ -438,7 +433,17 @@ void contract_pion_scatt_1spinorfield(spinor_field* phi_ts,spinor_field* phi_tsp
 			corr_im[l][t] = 0.;	
 			if ( l < 2 )  corr_tmp[l][t]=0.;
 		}	
-		corr_2pt[t]=0.;  
+		corr_2pt[t]=0.; 
+		_complex_0(A1[t]);
+		_complex_0(A2[t]);
+		_complex_0(B1[t]);
+		_complex_0(B2[t]);
+		_complex_0(C1[t]);
+		_complex_0(C2[t]);
+		_complex_0(D1[t]);
+		_complex_0(D2[t]);
+
+
 	}	
 
 
@@ -446,13 +451,6 @@ void contract_pion_scatt_1spinorfield(spinor_field* phi_ts,spinor_field* phi_tsp
 
 	for (t=0; t<T; t++)
 	{
-		//	  if(COORD[0]==((t+ts)%GLB_T)/T) {// Check that t + ts is in this thread.	
-
-		A1.re=0;A1.im=0.;A2.re=0;A2.im=0.;
-		B1.re=0;B1.im=0.;B2.re=0;B2.im=0.;
-		C1.re=0;C1.im=0.;C2.re=0;C2.im=0.;
-		D1.re=0;D1.im=0.;D2.re=0;D2.im=0.;
-
 
 		for (beta=0;beta<4;beta++)
 		{
@@ -463,83 +461,81 @@ void contract_pion_scatt_1spinorfield(spinor_field* phi_ts,spinor_field* phi_tsp
 			}
 		}
 
-		/* what is tc and offset ? MPI related ? */
 		tc = (zerocoord[0]+t+GLB_T-ts)%GLB_T;
 
 		/* loop on spatial volume */
 		for (x=0; x<X; x++) for (y=0; y<Y; y++) for (z=0; z<Z; z++) {
 
-			ix_pts_pa = ipt((t+ts+1)%GLB_T,x,y,z);
-			ix_pts = ipt((t+ts)%GLB_T,x,y,z);
-			// NOTE THAT IT PROBABLY WONT WORK WITH MPI (at least if the number of proc in the time direction is different from 0) !!
+			ix_pts = ipt(t,x,y,z);
+			ix_pts_pa = iup(ix_pts,0);
 
 			// A term
-			_spinor_prod_assign_g(A1, *_FIELD_AT(phi_tsp1,ix_pts_pa), *_FIELD_AT(phi_tsp1,ix_pts_pa));
-			_spinor_prod_assign_g(A2, *_FIELD_AT(phi_ts,ix_pts), *_FIELD_AT(phi_ts,ix_pts));
-
-
+			_spinor_prod_assign_f(A1[tc], *_FIELD_AT(phi_tsp1,ix_pts_pa), *_FIELD_AT(phi_tsp1,ix_pts_pa));
+			_spinor_prod_assign_f(A2[tc], *_FIELD_AT(phi_ts,ix_pts), *_FIELD_AT(phi_ts,ix_pts));
 
 			// B term
-
-			_spinor_prod_assign_g(B1, *_FIELD_AT(phi_ts,ix_pts_pa), *_FIELD_AT(phi_tsp1,ix_pts_pa));
-			_spinor_prod_assign_g(B2, *_FIELD_AT(phi_tsp1,ix_pts), *_FIELD_AT(phi_ts,ix_pts));
+			_spinor_prod_assign_f(B1[tc], *_FIELD_AT(phi_ts,ix_pts_pa), *_FIELD_AT(phi_tsp1,ix_pts_pa));
+			_spinor_prod_assign_f(B2[tc], *_FIELD_AT(phi_tsp1,ix_pts), *_FIELD_AT(phi_ts,ix_pts));
 
 			// C term   : useless C^ast = B
-
-			_spinor_prod_assign_g(C1, *_FIELD_AT(phi_ts,ix_pts_pa), *_FIELD_AT(phi_tsp1,ix_pts_pa));
-			_spinor_prod_assign_g(C2, *_FIELD_AT(phi_tsp1,ix_pts), *_FIELD_AT(phi_ts,ix_pts));
+			_spinor_prod_assign_f(C1[tc], *_FIELD_AT(phi_tsp1,ix_pts_pa), *_FIELD_AT(phi_ts,ix_pts_pa));
+			_spinor_prod_assign_f(C2[tc], *_FIELD_AT(phi_ts,ix_pts), *_FIELD_AT(phi_tsp1,ix_pts));
 
 
 			// D term
-			_spinor_prod_assign_g(D1, *_FIELD_AT(phi_ts,ix_pts_pa), *_FIELD_AT(phi_ts,ix_pts_pa));
-			_spinor_prod_assign_g(D2, *_FIELD_AT(phi_tsp1,ix_pts), *_FIELD_AT(phi_tsp1,ix_pts));
+			_spinor_prod_assign_f(D1[tc], *_FIELD_AT(phi_ts,ix_pts_pa), *_FIELD_AT(phi_ts,ix_pts_pa));
+			_spinor_prod_assign_f(D2[tc], *_FIELD_AT(phi_tsp1,ix_pts), *_FIELD_AT(phi_tsp1,ix_pts));
 
 		}/* end loops on space */
 
-		_complex_mul_assign(corr[0][tc],A1,A2);
-		_complex_mul_assign(corr[1][tc],B1,B2);
-		_complex_mul_assign(corr[2][tc],C1,C2);
-		_complex_mul_assign(corr[3][tc],D1,D2);
 
-		corr_tmp[0][tc] = A1.re;
-		corr_tmp[1][tc] = A2.re;
+		corr_tmp[0][tc] = A1[tc].re;
+		corr_tmp[1][tc] = A2[tc].re;
 
 	} /* end loop on time */
 
+	//global sums	
+	global_sum((double*)A1,2*GLB_T);
+	global_sum((double*)A2,2*GLB_T);
+	global_sum((double*)B1,2*GLB_T);
+	global_sum((double*)B2,2*GLB_T);
+	global_sum((double*)C1,2*GLB_T);
+	global_sum((double*)C2,2*GLB_T);
+	global_sum((double*)D1,2*GLB_T);
+	global_sum((double*)D2,2*GLB_T);
 
 
-	// split real and imaginary part of the correlators
+	// build correlators
+	for(t=0;t<GLB_T;++t) {
+		_complex_mul(corr[0][t],A1[t],A2[t]); 
+		_complex_mul(corr[1][t],B1[t],B2[t]); 
+		_complex_mul(corr[2][t],C1[t],C2[t]); 
+		_complex_mul(corr[3][t],D1[t],D2[t]);
+	}
 
+	// normalize
+	for (l=0;l<4;l++) {
+		if (l==1 || l ==2) norm=-GLB_VOL3*GLB_VOL3; // B and C term have a global  minus sign  in the contraction
+		else	  norm= GLB_VOL3*GLB_VOL3;	 // A and B
+		for(t=0;t<GLB_T;++t) {
+			corr[l][t].re = corr[l][t].re/norm;
+			corr[l][t].im = corr[l][t].im/norm;
+		}
+	}
+
+	// build two point
 	for (t=0;t<T;t++){
 
 		tc = (zerocoord[0]+t+GLB_T-ts)%GLB_T;
-
-		for (l=0;l<4;l++) {
-
-			if (l==1 || l ==2) norm=-GLB_VOL3*GLB_VOL3; // B and C term have a global  minus sign  in the contraction
-			else	  norm= GLB_VOL3*GLB_VOL3;	 // A and B
-
-			corr_re[l][tc]=corr[l][tc].re/norm;
-			corr_im[l][tc]=corr[l][tc].im/norm;
-		}
-
-
-		// printf("%i %i %3.10e %3.10e \n",tc,(tc+GLB_T)%GLB_T,corr_tmp[0][(tc+GLB_T)%GLB_T],corr_tmp[1][tc]);
-		corr_2pt[tc] = 0.5 *(corr_tmp[0][(tc+GLB_T)%GLB_T]+corr_tmp[1][tc])/(GLB_VOL3);
-		// corr_2pt[tc] =(corr_tmp[1][tc])/(GLB_X*GLB_Y*GLB_Z);
+		corr_2pt[tc] = 0.5 *(corr_tmp[0][tc]+corr_tmp[1][tc])/(GLB_VOL3);
+		//		corr_2pt[tc] =  (corr_tmp[1][tc])/(GLB_VOL3);
 
 	}
-
-
-	/* global sums */
-	for (l=0;l<4;l++) {
-		global_sum(corr_re[l],GLB_T);
-		global_sum(corr_im[l],GLB_T);
-	}
+	// global sum 2pt
 	global_sum(corr_2pt,GLB_T);
 
 	lprintf("TEST_PP",0," Correlators C_pipi : T l nhits re im ! \n");
-	for(l=0;l<4;l++ ) for(t=0;t<GLB_T;++t) lprintf("TEST_PP",0,"%i %i %i %3.10e %3.10e \n",t,l,k,corr_re[l][t],corr_im[l][t]);
+	for(l=0;l<4;l++ ) for(t=0;t<GLB_T;++t) lprintf("TEST_PP",0,"%i %i %i %3.10e %3.10e \n",t,l,k,corr[l][t].re,corr[l][t].im);
 
 	lprintf("TEST_P",0," Correlators C_pi : T  nhits re ! \n");
 	for(t=0;t<GLB_T;++t)  lprintf("TEST_P",0,"%i %i %3.10e  \n",t,k,corr_2pt[t]);
@@ -548,5 +544,5 @@ void contract_pion_scatt_1spinorfield(spinor_field* phi_ts,spinor_field* phi_tsp
 	timeval_subtract(&etime,&end,&start);
 	lprintf("TIMING",0,"Contractions for Pion-Pion ( nhits = %i ) scattering done in  [%ld sec %ld usec]\n",k,etime.tv_sec,etime.tv_usec);
 
-	}
+}
 
