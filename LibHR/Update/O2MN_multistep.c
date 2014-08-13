ab83d5b97f29194b0cb62d02c12fbd2404b5b1cc
Claudio Pica <pica@cp3.sdu.dk>
2014-08-13 09:07:43 +0000

    Included fourth order OMF integrator in O2MN_multistep. To be tested yet...
    git-svn-id: file:///Users/pica/Downloads/GitClone/svnrep/suN/HiRep@642 37a69121-b030-0410-a54a-fdca83ffee47

--- LibHR/Update/O2MN_multistep.c
+++ LibHR/Update/O2MN_multistep.c
@@ -181,6 +181,81 @@ void O2MN_multistep(suNg_av_field *momenta, double tlen, integrator_par *int_par
 }
 
 
+/* 4th order  I.P. Omelyan, I.M. Mryglod, R. Folk, computer Physics Communications 151 (2003) 272-314 */
+
+const double r1=0.08398315262876693;
+const double r2=0.2539785108410595;
+const double r3=0.6822365335719091;
+const double r4=-0.03230286765269967;
+
+void O4MN_multistep(suNg_av_field *momenta, double tlen, integrator_par *int_par){
+#ifdef TIMING
+  struct timeval start, end;
+  struct timeval etime;
+  
+#ifdef TIMING_WITH_BARRIERS
+  MPI_Barrier(GLB_COMM);
+#endif
+  gettimeofday(&start,0);
+#endif
+  
+  /* check input types */
+#ifndef CHECK_SPINOR_MATCHING
+  _TWO_SPINORS_MATCHING(u_gauge,momenta);
+#endif
+  
+  if(int_par->nsteps == 0)  return;
+  
+  int n;
+  double dt=tlen/((double)int_par->nsteps);
+  
+  lprintf("MD_INT",10+int_par->level*10,"Starting new MD trajectory with O2MN_multistep, level %d.\n",int_par->level);
+  lprintf("MD_INT",10+int_par->level*10,"MD parameters: level=%d tlen=%1.6f nsteps=%d => dt=%1.6f\n",
+          int_par->level,tlen,int_par->nsteps,dt);
+  
+  (*int_par->force)(r1*dt,momenta,int_par->force_par);
+  
+  for(n=1;n<=int_par->nsteps;++n) {
+    
+    (*int_par->next->integrator)(momenta, r2*dt, int_par->next);
+    
+    (*int_par->force)(r3*dt,momenta,int_par->force_par);
+
+    (*int_par->next->integrator)(momenta, r4*dt, int_par->next);
+
+    (*int_par->force)((0.5-r1-r3)*dt,momenta,int_par->force_par);
+
+    (*int_par->next->integrator)(momenta, (1.-2.*(r2+r4))*dt, int_par->next);
+
+    (*int_par->force)((0.5-r1-r3)*dt,momenta,int_par->force_par);
+    
+    (*int_par->next->integrator)(momenta, r4*dt, int_par->next);
+
+    (*int_par->force)(r3*dt,momenta,int_par->force_par);
+   
+    (*int_par->next->integrator)(momenta, r2*dt, int_par->next);
+
+    /* Update of momenta */
+    if(n<int_par->nsteps) {
+      (*int_par->force)(2.*r1*dt,momenta,int_par->force_par);
+      lprintf("MD_INT",20+int_par->level*10,"MD step (level %d): %d/%d\n",int_par->level,n,int_par->nsteps);
+    } else {
+      (*int_par->force)(r1*dt,momenta,int_par->force_par);
+      lprintf("MD_INT",10+int_par->level*10,"MD trajectory completed, level %d.\n",int_par->level);
+    }
+  }
+  
+#ifdef TIMING
+#ifdef TIMING_WITH_BARRIERS
+  MPI_Barrier(GLB_COMM);
+#endif
+  gettimeofday(&end,0);
+  timeval_subtract(&etime,&end,&start);
+  lprintf("TIMING",0,"O2MN_multistep[%d] %.6f s\n",int_par->level,1.*etime.tv_sec+1.e-6*etime.tv_usec);
+#endif
+}
+
+
 
 
 #undef _PROJ_BIT
