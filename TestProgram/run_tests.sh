5aa2e3716e795714704d15cce9919c9e5ce3454f
Claudio Pica <pica@cp3.sdu.dk>
2019-10-19 14:46:42 +0200

    debug actions

--- TestProgram/run_tests.sh
+++ TestProgram/run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# if we are running inside a github action, change workdir
+[! -z "$GITHUB_WORKSPACE" ] && cd $GITHUB_WORKSPACE/TestProgram
+
 ../Make/Utils/write_mkflags.pl -f ../Make/MkFlags $@
 
 echo Cleaning...
