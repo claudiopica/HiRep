a4a4761ef52454ce74005a3c1101999896b2c598
Antonio Rago <antonio.rago@plymouth.ac.uk>
2020-12-18 21:41:08 +0000

    test new actions

--- TestProgram/run_tests.sh
+++ TestProgram/run_tests.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -e
+while getopts 'h' c
+do
+  case $c in
+    h) echo heko ;
+      ../Make/Utils/write_mkflags.pl -h
+    ;;
+  esac
+done
+
+[ ! -d "$1" ] && echo First argument must be a subdirectory of TestProgram && exit 1
 
 # if we are running inside a github action, change workdir
 [ ! -z "$GITHUB_WORKSPACE" ] && cd $GITHUB_WORKSPACE/TestProgram
 
-../Make/Utils/write_mkflags.pl -f ../Make/MkFlags $@ || exit 1
+../Make/Utils/write_mkflags.pl -f ../Make/MkFlags ${@: 2} || exit 1
 
 echo Cleaning...
 ( cd .. && make cleanall )
 
+cd ./${1}
+
 echo Building...
-make -j1 tests
+make -j1 
 
 echo Run Tests...
-make runalltests
+make runtests
