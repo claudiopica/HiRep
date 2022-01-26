406347f8d92d8d7adb52908b168843b92b4515e4
Antonio Rago <rago@foseres.localdomain>
2022-01-26 15:01:43 +0000

    missing file to the previous submission

--- Include/geometry.h
+++ Include/geometry.h
@@ -12,13 +12,14 @@
 
 #define BORDERSIZE 1
 
-typedef struct _geometry_descriptor {
+typedef struct _geometry_descriptor
+{
   int inner_master_pieces; /* number of inner pieces (1 o 2 for even odd or no_eo)) */
-  int local_master_pieces;  // 
+  int local_master_pieces; //
   int total_spinor_master_pieces;
   int total_gauge_master_pieces;
   int *master_start, *master_end; // Beginning has inner pieces, then local pieces, then the rest.
-  int master_shift; /*  this is the odd spinor's shift, i.e. the index of the first odd entry in the full geometry */
+  int master_shift;               /*  this is the odd spinor's shift, i.e. the index of the first odd entry in the full geometry */
   int ncopies_spinor;
   int ncopies_gauge;
   int *copy_from, *copy_to, *copy_len;
@@ -35,63 +36,51 @@ typedef struct _geometry_descriptor {
   int fuse_inner_counter;
 } geometry_descriptor;
 
-
 #include "hr_omp.h"
 
 //Loop over pieces of given type
-#define _PIECE_FOR(type,ip) \
-  for(int ip=0;\
-      ip<(type)->local_master_pieces;\
-      ip++ )
+#define _PIECE_FOR(type, ip) \
+  for (int ip = 0; ip < (type)->local_master_pieces; ip++)
 
 //Loop over sites of piece ip of given type
-#define _SITE_FOR_RED(type,ip,is,redop1,redop2) \
-_OMP_PRAGMA ( _omp_parallel )\
-_OMP_PRAGMA ( _omp_for redop1 redop2  )\
-  for(int is=(type)->master_start[ip]; \
-      is<=(type)->master_end[ip]; \
-      is++ )
+#define _SITE_FOR_RED(type, ip, is, redop1, redop2) \
+  _OMP_PRAGMA(_omp_parallel)                        \
+  _OMP_PRAGMA(_omp_for redop1 redop2)               \
+  for (int is = (type)->master_start[ip]; is <= (type)->master_end[ip]; is++)
 
-#define _SITE_FOR(type,ip,is) _SITE_FOR_RED(type,ip,is,nowait,)
-#define _SITE_FOR_SUM(type,ip,is,...) _SITE_FOR_RED(type,ip,is,_omp_sum(__VA_ARGS__),)
-#define _SITE_FOR_MAX(type,ip,is,...) _SITE_FOR_RED(type,ip,is,_omp_max(__VA_ARGS__),)
-#define _SITE_FOR_MIN(type,ip,is,...) _SITE_FOR_RED(type,ip,is,_omp_min(__VA_ARGS__),)
-
+#define _SITE_FOR(type, ip, is) _SITE_FOR_RED(type, ip, is, nowait, )
+#define _SITE_FOR_SUM(type, ip, is, ...) _SITE_FOR_RED(type, ip, is, _omp_sum(__VA_ARGS__), )
+#define _SITE_FOR_MAX(type, ip, is, ...) _SITE_FOR_RED(type, ip, is, _omp_max(__VA_ARGS__), )
+#define _SITE_FOR_MIN(type, ip, is, ...) _SITE_FOR_RED(type, ip, is, _omp_min(__VA_ARGS__), )
 
 //Loop over sites of all pieces of given type
 //do an openmp sum over the variables in the redop list
-#define _MASTER_FOR_RED(type,is,redop1,redop2) \
-  _PIECE_FOR((type),_master_for_ip_##is)    \
-  _SITE_FOR_RED((type),_master_for_ip_##is,is,redop1,redop2)
+#define _MASTER_FOR_RED(type, is, redop1, redop2) \
+  _PIECE_FOR((type), _master_for_ip_##is)         \
+  _SITE_FOR_RED((type), _master_for_ip_##is, is, redop1, redop2)
 
-#define _MASTER_FOR(type,is) _MASTER_FOR_RED(type,is,,)
-#define _MASTER_FOR_SUM(type,is,...) _MASTER_FOR_RED(type,is,_omp_sum(__VA_ARGS__),)
-#define _MASTER_FOR_MAX(type,is,...) _MASTER_FOR_RED(type,is,_omp_max(__VA_ARGS__),)
-#define _MASTER_FOR_MIN(type,is,...) _MASTER_FOR_RED(type,is,_omp_min(__VA_ARGS__),)
+#define _MASTER_FOR(type, is) _MASTER_FOR_RED(type, is, , )
+#define _MASTER_FOR_SUM(type, is, ...) _MASTER_FOR_RED(type, is, _omp_sum(__VA_ARGS__), )
+#define _MASTER_FOR_MAX(type, is, ...) _MASTER_FOR_RED(type, is, _omp_max(__VA_ARGS__), )
+#define _MASTER_FOR_MIN(type, is, ...) _MASTER_FOR_RED(type, is, _omp_min(__VA_ARGS__), )
 
-#define _FUSE_IDX(type,is) (type)->fuse_mask[_fuse_master_for_ip_##is]
+#define _FUSE_IDX(type, is) (type)->fuse_mask[_fuse_master_for_ip_##is]
 
-#define _FUSE_FOR_RED(type,ip,is,redop1,redop2) \
-int is;\
-_OMP_PRAGMA ( _omp_parallel )\
-_OMP_PRAGMA ( _omp_for redop1 redop2  )\
-  for(int ip=0 ; \
-      ip<type->fuse_gauge_size; \
-      ip++) 
+#define _FUSE_FOR_RED(type, ip, is, redop1, redop2) \
+  _OMP_PRAGMA(_omp_parallel)                        \
+  _OMP_PRAGMA(_omp_for redop1 redop2)               \
+  for (int ip = 0; ip < type->fuse_gauge_size; ip++)
 
+#define _FUSE_MASTER_FOR_RED(type, is, redop1, redop2) \
+  _FUSE_FOR_RED((type), _fuse_master_for_ip_##is, is, redop1, redop2)
 
-#define _FUSE_MASTER_FOR_RED(type,is,redop1,redop2) \
-  _FUSE_FOR_RED((type),_fuse_master_for_ip_##is,is,redop1,redop2)
-
-#define _FUSE_MASTER_FOR(type,is) _FUSE_MASTER_FOR_RED(type,is,,)
-#define _FUSE_MASTER_FOR_SUM(type,is,...) _FUSE_MASTER_FOR_RED(type,is,_omp_sum(__VA_ARGS__),)
-#define _FUSE_MASTER_FOR_MAX(type,is,...) _FUSE_MASTER_FOR_RED(type,is,_omp_max(__VA_ARGS__),)
-#define _FUSE_MASTER_FOR_MIN(type,is,...) _FUSE_MASTER_FOR_RED(type,is,_omp_min(__VA_ARGS__),)
-
-
+#define _FUSE_MASTER_FOR(type, is) _FUSE_MASTER_FOR_RED(type, is, , )
+#define _FUSE_MASTER_FOR_SUM(type, is, ...) _FUSE_MASTER_FOR_RED(type, is, _omp_sum(__VA_ARGS__), )
+#define _FUSE_MASTER_FOR_MAX(type, is, ...) _FUSE_MASTER_FOR_RED(type, is, _omp_max(__VA_ARGS__), )
+#define _FUSE_MASTER_FOR_MIN(type, is, ...) _FUSE_MASTER_FOR_RED(type, is, _omp_min(__VA_ARGS__), )
 
 void origin_coord(int *c);
-void other_proc_origin_coord(int * proc_coord, int *c);
+void other_proc_origin_coord(int *proc_coord, int *c);
 void glb_to_proc(int *g, int *p);
 
 int geometry_init(void);
@@ -102,11 +91,9 @@ int proc_dn(int id, int dir);
 
 void free_geometry_mpi_eo(void);
 
-
 void init_geometry_SAP(void);
 void test_geometry_mpi(void);
 void test_geometry_mpi_eo(void);
 void print_wdmatrix(char *filename);
-
 
 #endif
