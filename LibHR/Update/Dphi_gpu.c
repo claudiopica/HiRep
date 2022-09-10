e72b0e2ac86189deee4197d31dff9e40ef4006a8
Sofie Martins <martinss@imada.sdu.dk>
2022-09-10 21:44:04 +0200

    Added geometry test for gauge field. Test succeeds.

--- LibHR/Update/Dphi_gpu.c
+++ LibHR/Update/Dphi_gpu.c
@@ -31,44 +31,6 @@
 #include "hr_complex.h"
 #include <iostream>
 
-void global_sum(double *d, int n) {
-#ifdef WITH_MPI
-  int mpiret;(void)mpiret; // Remove warning of variable set but not used
-  double pres[n];
-  
-#ifdef MPI_TIMING
-  struct timeval start, end, etime;
-  gettimeofday(&start,0);  
-#endif
-  
-  mpiret=MPI_Allreduce(d,pres,n,MPI_DOUBLE,MPI_SUM,GLB_COMM);
-  
-    
-#ifdef MPI_TIMING
-  gettimeofday(&end,0);
-  timeval_subtract(&etime,&end,&start);
-  lprintf("MPI TIMING",0,"global_sum " __FILE__ " %ld sec %ld usec\n",etime.tv_sec,etime.tv_usec);
-#endif
-  
-#ifndef NDEBUG
-  if (mpiret != MPI_SUCCESS) {
-    char mesg[MPI_MAX_ERROR_STRING];
-    int mesglen;
-    MPI_Error_string(mpiret,mesg,&mesglen);
-    lprintf("MPI",0,"ERROR: %s\n",mesg);
-    error(1,1,"global_sum " __FILE__,"Cannot perform global_sum");
-  }
-#endif
-  while(n>0) { 
-    --n;
-    d[n]=pres[n];	
-  }
-#else
-  /* for non mpi do nothing */
-  return;
-#endif
-}
-
 __global__ void Dphi_gpu_kernel(suNf_spinor*, 
                             const suNf_spinor*,
                             const suNf*, 
--- TestProgram/Geometry/check_gpu_geometry.c
+++ TestProgram/Geometry/check_gpu_geometry.c
@@ -12,21 +12,151 @@
 #include "geometry.h"
 
 int test_write_read_spinor_field_f();
+int test_write_read_gauge_field_f();
+//int test_write_read_gauge_field();
 
 int main(int argc, char *argv[]) 
 {
     // Init
-    int pass = 1;
+    int return_val = 0;
 
     // Setup process and communication
     logger_map("DEBUG", "debug");
     setup_process(&argc, &argv);
 
-    pass &= test_write_read_spinor_field_f();
-    
+    return_val += test_write_read_spinor_field_f();
+    return_val += test_write_read_gauge_field_f();
+    //return_val += test_write_read_gauge_field();
+
+
     // Finalize and return
     finalize_process();
-    return pass;
+    return return_val;
+}
+
+/*int test_write_read_gauge_suNg_av() 
+{
+    int vol4h = T*X*Y*Z/2;
+    int return_val = 0;
+    suNg_av_field *in, *gpu_format, *out;
+    in = alloc_avfield(&glattice);
+    out = alloc_avfield(&glattice);
+    gpu_format = alloc_avfield(&glattice);
+
+    suNg_algebra_vector in_vec, out_vec;
+    int dim = 4*sizeof(in->ptr)/sizeof(double);
+
+    _PIECE_FOR(in->type, ixp) 
+    {
+        _SITE_FOR(in->type, ixp, ix) 
+        {
+            for (int comp = 0; comp < dim; comp++) 
+            {
+                _suNg_av_read_gpu(vol4h, (*(in->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+                _suNg_av_read_gpu(vol4h, (*(out->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+            }
+        }
+        
+        
+
+}*/
+
+/*int test_write_read_gauge_field()
+{
+    int vol4h = T*X*Y*Z/2;
+    int return_val = 0;
+    suNg_field *in, *gpu_format, *out;
+    in = alloc_gfield(&glattice);
+    out = alloc_gfield(&glattice);
+    gpu_format = alloc_gfield(&glattice);
+
+    suNg in_mat, out_mat;
+    int dim = sizeof(in->ptr)/sizeof(double);
+    
+    _PIECE_FOR(in->type, ixp) 
+    {
+        _SITE_FOR(in->type, ixp, ix) 
+        {
+            for (int comp = 0; comp < dim; comp++) 
+            {
+                _suNg_write_gpu(vol4h, (*(in->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+                _suNg_read_gpu(vol4h, (*(out->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+            }
+        }
+    }
+
+    suNg_field_sub_assign_cpu(out, in);
+    double diff_norm = spinor_field_sqnorm_cpu(out);
+
+    // Since this is just a copy they have to be identical
+    if (diff_norm != 0) 
+    {
+        lprintf("RESULT", 0, "FAILED \n");
+        return_val = 1;
+    }
+    else 
+    {
+        lprintf("RESULT", 0, "OK \n");
+        return_val = 0;
+    }
+    lprintf("RESULT", 0, "[Diff norm %0.2e]\n", diff_norm);
+
+    free_gfield(in);
+    free_gfield(gpu_format);
+    free_gfield(out);
+    return return_val;
+}*/
+
+int test_write_read_gauge_field_f()
+{
+    int vol4h = T*X*Y*Z/2;
+    int return_val = 0;
+    double diff_norm = 0.0;
+    double sqnorm = 0.0;
+    suNf_field *in, *gpu_format, *out;
+    in = alloc_gfield_f(&glattice);
+    out = alloc_gfield_f(&glattice);
+    gpu_format = alloc_gfield_f(&glattice);
+
+    suNf in_mat, out_mat;
+    int dim = sizeof(in->ptr)/sizeof(double);
+    
+    _PIECE_FOR(in->type, ixp) 
+    {
+        _SITE_FOR(in->type, ixp, ix) 
+        {
+            for (int comp = 0; comp < dim; comp++) 
+            {
+                in_mat = *(in->ptr+ix);
+                out_mat = *(out->ptr+ix);
+                _suNf_write_gpu(vol4h, in_mat, gpu_format->ptr, ix, comp);
+                _suNf_read_gpu(vol4h, out_mat, gpu_format->ptr, ix, comp);
+
+
+                _suNf_sub_assign(out_mat, in_mat);
+                _suNf_sqnorm(sqnorm, out_mat);
+                diff_norm += sqnorm;
+            }
+        }
+    }
+
+    // Since this is just a copy they have to be identical
+    if (diff_norm != 0) 
+    {
+        lprintf("RESULT", 0, "FAILED \n");
+        return_val = 1;
+    }
+    else 
+    {
+        lprintf("RESULT", 0, "OK \n");
+        return_val = 0;
+    }
+    lprintf("RESULT", 0, "[Diff norm %0.2e]\n", diff_norm);
+
+    free_gfield_f(in);
+    free_gfield_f(gpu_format);
+    free_gfield_f(out);
+    return return_val;
 }
 
 int test_write_read_spinor_field_f()
@@ -47,8 +177,8 @@ int test_write_read_spinor_field_f()
         {
             for (int comp=0; comp < 4; comp++) 
             {
-                _suNf_write_spinor_gpu(vol4h/2, (*(in->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
-                _suNf_read_spinor_gpu(vol4h/2, (*(out->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+                _suNf_write_spinor_gpu(vol4h, (*(in->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+                _suNf_read_spinor_gpu(vol4h, (*(out->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
             }
         }
     }
@@ -56,7 +186,7 @@ int test_write_read_spinor_field_f()
     spinor_field_sub_assign_f_cpu(out, in);
     double diff_norm = spinor_field_sqnorm_f_cpu(out);
 
-    if (diff_norm > 1e-14) {
+    if (diff_norm != 0) {
         lprintf("RESULT", 0, "FAILED \n");
         return_val = 1;
     } 
@@ -65,9 +195,10 @@ int test_write_read_spinor_field_f()
         lprintf("RESULT", 0, "OK \n");
         return_val = 0;
     }
-    lprintf("RESULT", 0, "[Diff norm gpu-cpu %0.2e]\n", diff_norm);
+    lprintf("RESULT", 0, "[Diff norm %0.2e]\n", diff_norm);
 
     free_spinor_field_f(in);
+    free_spinor_field_f(gpu_format);
     free_spinor_field_f(out);
     return return_val;
 } 
