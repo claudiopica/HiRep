0052651854f0af005fd1cf8e57fa5deab772bcc6
Sofie Martins <martinss@imada.sdu.dk>
2022-09-10 22:18:03 +0200

    suNg_field gpu geometry test passes

--- TestProgram/Geometry/check_gpu_geometry.c
+++ TestProgram/Geometry/check_gpu_geometry.c
@@ -13,7 +13,7 @@
 
 int test_write_read_spinor_field_f();
 int test_write_read_gauge_field_f();
-//int test_write_read_gauge_field();
+int test_write_read_gauge_field();
 
 int main(int argc, char *argv[]) 
 {
@@ -24,47 +24,23 @@ int main(int argc, char *argv[])
     logger_map("DEBUG", "debug");
     setup_process(&argc, &argv);
 
+    // This only checks whether the reading/writing is bijective 
+    // when using them to operate on CPU memory
     return_val += test_write_read_spinor_field_f();
     return_val += test_write_read_gauge_field_f();
-    //return_val += test_write_read_gauge_field();
-
+    return_val += test_write_read_gauge_field();
 
     // Finalize and return
     finalize_process();
     return return_val;
 }
 
-/*int test_write_read_gauge_suNg_av() 
+int test_write_read_gauge_field()
 {
     int vol4h = T*X*Y*Z/2;
     int return_val = 0;
-    suNg_av_field *in, *gpu_format, *out;
-    in = alloc_avfield(&glattice);
-    out = alloc_avfield(&glattice);
-    gpu_format = alloc_avfield(&glattice);
-
-    suNg_algebra_vector in_vec, out_vec;
-    int dim = 4*sizeof(in->ptr)/sizeof(double);
-
-    _PIECE_FOR(in->type, ixp) 
-    {
-        _SITE_FOR(in->type, ixp, ix) 
-        {
-            for (int comp = 0; comp < dim; comp++) 
-            {
-                _suNg_av_read_gpu(vol4h, (*(in->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
-                _suNg_av_read_gpu(vol4h, (*(out->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
-            }
-        }
-        
-        
-
-}*/
-
-/*int test_write_read_gauge_field()
-{
-    int vol4h = T*X*Y*Z/2;
-    int return_val = 0;
+    double diff_norm = 0.0;
+    double sqnorm = 0.0;
     suNg_field *in, *gpu_format, *out;
     in = alloc_gfield(&glattice);
     out = alloc_gfield(&glattice);
@@ -79,14 +55,18 @@ int main(int argc, char *argv[])
         {
             for (int comp = 0; comp < dim; comp++) 
             {
-                _suNg_write_gpu(vol4h, (*(in->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
-                _suNg_read_gpu(vol4h, (*(out->ptr+ix)).c[comp], gpu_format->ptr, ix, comp);
+                in_mat = *(in->ptr+ix);
+                out_mat = *(out->ptr+ix);
+                _suNg_write_gpu(vol4h, in_mat, gpu_format->ptr, ix, comp);
+                _suNg_read_gpu(vol4h, out_mat, gpu_format->ptr, ix, comp);
+
+
+                _suNg_sub_assign(out_mat, in_mat);
+                _suNg_sqnorm(sqnorm, out_mat);
+                diff_norm += sqnorm;
             }
         }
     }
-
-    suNg_field_sub_assign_cpu(out, in);
-    double diff_norm = spinor_field_sqnorm_cpu(out);
 
     // Since this is just a copy they have to be identical
     if (diff_norm != 0) 
@@ -105,7 +85,7 @@ int main(int argc, char *argv[])
     free_gfield(gpu_format);
     free_gfield(out);
     return return_val;
-}*/
+}
 
 int test_write_read_gauge_field_f()
 {
