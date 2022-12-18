140e9ad3060f80c10515acd28322ee602d465e62
Sofie Martins <martinss@imada.sdu.dk>
2022-12-18 16:09:47 +0100

    More encapsulation for the memory function generation.

---
+++ LibHR/Memory/alloc_cpu_field_data.c
@@ -0,0 +1,55 @@
+/**
+ * @file alloc_cpu_field_data.c
+ * @brief Allocation code snippets for the CPU/host that can be added to allocation 
+ *        functions and are general to the field type.
+ */
+
+/**
+ * @brief Allocate the poiner to the spinor field structure.
+ *
+ * @param _name                     Name of the field, this should describe
+ *                                  the function this field has in the code
+ *                                  given the dimension (_size)
+ */
+#define _ALLOC_FIELD_STRUCT(_name)                                                                          \
+    f = amalloc(sizeof(*f), ALIGN);                                                                         \
+        error(f == NULL, 1, "alloc_" #_name " [" __FILE__ "]",                                              \
+                    "Could not allocate memory space for field (structure)");                               \
+        f->type = type;
+
+/**
+ * @brief Allocate space for the field data of the local lattice on the CPU/host.
+ *
+ * @param _name                     Name of the field, this should describe
+ *                                  the function this field has in the code
+ *                                  given the dimension (_size)
+ * @param _size                     Number of elementary site types saved
+ *                                  per site
+ * @param _geom                     The geometries are slightly different depending
+ *                                  on whether the field is located on the sites or
+ *                                  on the links. Fields that are located
+ *                                  on the sites are "spinor-like", so put 
+ *                                  'spinor' (without quotation marks), fields
+ *                                  that are located on the links are "gauge-like"
+ *                                  so put 'gauge'.
+ */
+#define _ALLOC_CPU_FIELD_DATA(_name, _size, _geom)                                                          \
+    if (alloc_mem_t & CPU_MEM)                                                                              \
+    {                                                                                                       \
+        /* For spinors: Allocate for all spinor array elements */                                           \
+        int bytes_per_site = sizeof(*(f->ptr));                                                             \
+        int number_of_sites = _n * (_size) * type->gsize_##_geom;                                           \
+        int field_size = bytes_per_site * number_of_sites;                                                  \
+                                                                                                            \
+        f->ptr = amalloc(field_size, ALIGN);                                                                \
+        error((f->ptr) == NULL, 1, "alloc_" #_name " [" __FILE__ "]",                                       \
+                    "Could not allocate memory space for field (data)");                                    \
+                                                                                                            \
+        /* For spinors: Map the elements of the spinor arrays to the */                                     \
+        /* starting points in the previously allocated space. */                                            \
+        for (int i = 1; i < _n; ++i)                                                                        \
+            f[i].ptr = f[i-1].ptr + type->gsize_##_geom * (_size);                                          \
+    }                                                                                                       \
+    else                                                                                                    \
+        for (int i = 0; i < _n; ++i)                                                                        \
+            f[i].ptr = NULL;
--- LibHR/Memory/field_alloc.c
+++ LibHR/Memory/field_alloc.c
@@ -3,13 +3,10 @@
 * All rights reserved.                                                      * 
 \***************************************************************************/
 
-/*******************************************************************************
-*
-* File gauge_field_alloc.c
-*
-* Functions for gauge field allocation and memory operations
-*
-*******************************************************************************/
+/**
+ * @file field_alloc.c
+ * @brief Allocation and free functions for all fields defined in spinor_field.h
+ */
 
 #include <stdlib.h>
 #include "suN.h"
@@ -25,152 +22,26 @@
 #include <mpi.h>
 #endif
 
-// TODO: doxygen docstrings
-// TODO: inline comments
-// TODO: More encapsulation?
+#include "alloc_cpu_field_data.c"
+#include "alloc_gpu_field_data.c"
+#include "alloc_mpi_data.c"
 
-/*
- * Here we use for all macros the parameters:
+/**
+ * @brief Generate free function for all fields defined in spinor_field.h
+ *        considering whether HiRep is compiled WITH_GPU and/or WITH_MPI.
  *
- *  _name = suffix of the allocation and deallocation functions
- *  _field_type = field type to allocate/deallocate
- *  _site_type = type of elementary objects on the lattice sites
- *  _size = the number of elementary objects per lattice site
- *
+ * @param _name                     Name of the field, this should describe
+ *                                  the function this field has in the code
+ *                                  given the dimension (_size)
+ * @param _field_type               Type of field, use definitions in
+ *                                  spinor_field.h
+ * @param _site_type                Elementary site type from suN.h
  */
-
-/* ================================================= MPI ================================================= */
-
-#if defined(WITH_MPI)
-
-        #define _FREE_MPI_CODE  if (u->comm_req != NULL) afree(u->comm_req)
-
-        #ifdef WITH_NEW_GEOMETRY
-            #ifdef WITH_GPU
-                #define _SENDBUF_ALLOC(_size, _i) \
-                /*TODO: GPU sendbuf not allocated correctly. (SAM)*/ \
-                    f[_i].sendbuf_ptr = sendbuf_alloc((_size)*sizeof(*(f[_i].ptr))); \
-                    int alloc_length = (_size)*sizeof(*(f[_i].ptr))*(glattice.gsize_gauge - boxVolume(geometryBoxes)); \
-                    cudaMalloc((void **)&(f[_i].sendbuf_gpu_ptr), alloc_length);
-            #else
-                #define _SENDBUF_ALLOC(_size, _i) \
-                    f[_i].sendbuf_ptr = sendbuf_alloc((_size)*sizeof(*(f[_i].ptr)));
-            #endif
-        #else
-            #ifdef WITH_GPU
-                #define _SENDBUF_ALLOC(_size, _i) \
-                    f[_i].sendbuf_ptr = f[_i].ptr; \
-                    f[_i].sendbuf_gpu_ptr = f[_i].gpu_ptr;
-            #else
-                #define _SENDBUF_ALLOC(_size, _i) \
-                    f[_i].sendbuf_ptr = f[_i].ptr; 
-            #endif
-        #endif
-
-
-        #define _FREE_MPI_FIELD_DATA                                                                        \
-            if (f->comm_req != NULL)                                                                        \
-                afree(f->comm_req)                                                                          \
-
-        #define _ALLOC_MPI_FIELD_DATA(_name, _size, _geom)                                                  \
-            if (type->nbuffers_##_geom > 0)                                                                 \
-            {                                                                                               \
-                f->comm_req = amalloc(_n * 2 * type->nbuffers_##_geom * sizeof(MPI_Request), ALIGN);        \
-                error((f->comm_req) == NULL, 1, "alloc_" #_name " [" __FILE__ "]",                          \
-                    "Could not allocate memory space for field (MPI)");                                     \
-                for (int ix = 0; ix < _n * 2 * type->nbuffers_##_geom; ++ix)                                \
-                    f->comm_req[ix] = MPI_REQUEST_NULL;                                                     \
-                for (int i = 1; i < _n; ++i)                                                                 \
-                {                                                                                           \
-                    f[i].comm_req = f[i-1].comm_req + 2 * type->nbuffers_##_geom;                           \
-                }                                                                                           \
-                for (int i = 1; i < _n; ++i)                                                                 \
-                {                                                                                           \
-                    _SENDBUF_ALLOC(_size, i);                                                               \
-                }                                                                                           \
-                                                                                                            \
-            }                                                                                               \
-            else                                                                                            \
-            {                                                                                               \
-                f->comm_req = NULL;                                                                         \
-            }                                                                                               \
-  
-#endif
-
-/* ================================================= GPU ================================================= */
-
-#if defined(WITH_GPU)
-
-        /* Free device memory */
-        /* Note: to be used inside function declaration */
-        #define _FREE_GPU_FIELD_DATA(_name, _site_type)                                                     \
-            if (f->gpu_ptr != NULL)                                                                         \
-                cudaFree(f->gpu_ptr);                                                                       \
-
-        /* Allocate device memory */
-        /* Note: to be used inside function declaration */
-        #define _ALLOC_GPU_FIELD_DATA(_name, _site_type, _size, _geom)                                             \
-            if (alloc_mem_t & GPU_MEM)                                                                      \
-            {                                                                                               \
-                cudaError_t err;                                                                            \
-                int field_size = _n * _size * type->gsize_##_geom * sizeof(*(f->gpu_ptr));                         \
-                err = cudaMalloc((void **)&(f->gpu_ptr), field_size);                                       \
-                error(err != cudaSuccess, 1, "alloc_" #_name " [" __FILE__ "]",                             \
-                                "Could not allocate GPU memory space for field");                           \
-                for (int i = 1; i < _n; ++i)                                                                \
-                    f[i].gpu_ptr = f[i-1].gpu_ptr + type->gsize_spinor * _size;                             \
-            }                                                                                               \
-            else                                                                                            \
-                for (int i = 0; i < _n; ++i)                                                                \
-                    f[i].gpu_ptr = NULL;
-
-#endif
-
-/* ================================================= Empty defs ========================================== */
-
-#ifndef WITH_GPU
-
-    #define _FREE_GPU_FIELD_DATA(_name, _site_type) do {} while (0)
-    #define _ALLOC_GPU_FIELD_DATA(_name, _site_type, _size, _geom) do {} while (0)
-
-#endif
-
-#ifndef WITH_MPI
-
-    #define _FREE_MPI_FIELD_DATA do {} while (0)
-    #define _ALLOC_MPI_FIELD_DATA(_name, _size, _geom) do {} while (0)
-
-#endif
-
-/* ============================================== CPU ==================================================== */
-
-#define _ALLOC_FIELD_STRUCT(_name)                                                                   \
-    f = amalloc(sizeof(*f), ALIGN);                                                                         \
-        error(f == NULL, 1, "alloc_" #_name " [" __FILE__ "]",                                              \
-                    "Could not allocate memory space for field (structure)");                               \
-        f->type = type;
-
-#define _ALLOC_CPU_FIELD_DATA(_name, _size, _geom)                                                                 \
-    if (alloc_mem_t & CPU_MEM)                                                                              \
-    {                                                                                                       \
-        int field_size = _n * (_size) * type->gsize_##_geom * sizeof(*(f->ptr));                            \
-        f->ptr = amalloc(field_size, ALIGN);                                                                \
-        error((f->ptr) == NULL, 1, "alloc_" #_name " [" __FILE__ "]",                                       \
-                    "Could not allocate memory space for field (data)");                                    \
-        for (int i = 1; i < _n; ++i)                                                                        \
-            f[i].ptr = f[i-1].ptr + type->gsize_##_geom * (_size);                                           \
-    }                                                                                                       \
-    else \
-    { \
-        for (int i = 0; i < _n; ++i)  \
-        { \
-            f[i].ptr = NULL; \
-        } \
-    }
-
-/* ============================================== All cases ============================================== */
-
 #define _DECLARE_FREE_FUNC(_name, _field_type, _site_type)                                                  \
+    /** 
+	  @brief Free field data, other struct fields and struct pointer of field struct. 
+	  @param _field_type		Field to be freed. \
+	 */ \
     void free_##_name(_field_type *f)                                                                       \
     {                                                                                                       \
         if (f != NULL)                                                                                      \
@@ -184,8 +55,23 @@
         }                                                                                                   \
     }
 
+/**
+ * @brief Generate allocation function for all fields defined in spinor_field.h
+ *        considering whether HiRep is compiled WITH_GPU and/or WITH_MPI.
+ *
+ * @param _name                     Name of the field, this should describe
+ *                                  the function this field has in the code
+ *                                  given the dimension (_size)
+ * @param _field_type               Type of field, use definitions in 
+ *                                  spinor_field.h
+ * @param _site_type                Elementary site type from suN.h
+ */
 #define _DECLARE_ALLOC_FUNC(_name, _field_type, _site_type, _size, _geom)                                   \
-    _field_type *_ALLOCATE(_name)                                                                                  \
+    /** 
+	  @brief Allocate field struct pointer 
+	  @param geometry_descriptor	Underlying lattice geometry to allocate on.
+	 */ \
+    _field_type *_ALLOCATE(_name)                                                                           \
     {                                                                                                       \
         _field_type *f;                                                                                     \
                                                                                                             \
@@ -197,10 +83,34 @@
         return f;                                                                                           \
     }
 
+/**
+ * @brief Use this macro to declare both allocation and free functions
+ *        for the given field type.
+ *
+ * @param _name                     Name of the field, this should describe the
+ *                                  function this field has in the code
+ *                                  given the dimension (_size)
+ * @param _field_type               Type of field, use definitions in 
+ *                                  spinor_field.h
+ * @param _site_type                Elementary site type from suN.h
+ * @param _size                     Number of elementary site types saved
+ *                                  per site
+ * @param _geom                     The geometries are slightly different depending 
+ *                                  on whether the field is located on the sites
+ *                                  or on the links. Fields that are located
+ *                                  on the sites are "spinor-like", so put 
+ *                                  'spinor' (without quotation marks), fields
+ *                                  that are located on the links are "gauge-like"
+ *                                  so put 'gauge'.
+ */
 #define _DECLARE_MEMORY_FUNC(_name, _field_type, _site_type, _size, _geom)                                  \
     _DECLARE_FREE_FUNC(_name, _field_type, _site_type)                                                      \
     _DECLARE_ALLOC_FUNC(_name, _field_type, _site_type, _size, _geom)                                              
 
+/* Spinor allocation and free */
+// We need to use the macros _n and _ALLOCATE here, because spinor-like 
+// fields have the additional parameter n, that is also and argument to
+// allocation.
 #define _n n
 #define _ALLOCATE(_name) alloc_##_name(unsigned int n, geometry_descriptor *type)
 _DECLARE_MEMORY_FUNC(spinor_field_f, spinor_field, suNf_spinor, 1, spinor);
@@ -209,6 +119,7 @@ _DECLARE_MEMORY_FUNC(sfield, scalar_field, double, 1, spinor);
 #undef _n
 #undef _ALLOCATE
 
+/* Gauge allocation and free */
 #define _n 1
 #define _ALLOCATE(_name) alloc_##_name(geometry_descriptor *type) 
 _DECLARE_MEMORY_FUNC(gfield, suNg_field, suNg, 4, gauge);
