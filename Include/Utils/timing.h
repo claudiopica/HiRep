2ce761efc6c54204fb606e810471ebb325c698cc
Claudio Pica <pica@cp3.sdu.dk>
2023-02-17 18:50:53 +0100

    fixing wrong header

--- Include/Utils/timing.h
+++ Include/Utils/timing.h
@@ -1,6 +1,12 @@
 #ifndef TIMING_H
 #define TIMING_H
 
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
+
 #include <sys/time.h>
 #include <time.h>
 
