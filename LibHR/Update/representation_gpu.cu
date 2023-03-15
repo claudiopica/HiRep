7cf742ce15eda188c4ee0880a646e18f2e5c2ad5
Sofie Martins <martinss@imada.sdu.dk>
2023-03-15 05:03:50 +0100

    Fixed bug

--- LibHR/Update/representation_gpu.cu
+++ LibHR/Update/representation_gpu.cu
@@ -20,19 +20,15 @@
 #ifdef WITH_GPU
 
 __global__ void represent_gauge_field_kernel(suNf *gauge_f, suNg *gauge, int N, int block_start) {
-    suNf Ru;
-    suNg u;
-
-    int ix = threadIdx.x * blockDim.x + threadIdx.x;
-    if (ix < N) {
-        ix += block_start;
+    for (int id = blockIdx.x * blockDim.x + threadIdx.x; id < N; id += gridDim.x * blockDim.x) {
+        suNf Ru;
+        suNg u;
+        int ix = id + block_start;
         read_gpu<double>(0, &u, gauge, ix, 0, 4);
         _group_represent2(&Ru, &u);
         write_gpu<double>(0, &u, gauge_f, ix, 0, 4);
     }
 }
-
-extern "C" {
 
 void represent_gauge_field_gpu() {
     _PIECE_FOR(u_gauge_f->type, ixp) {
@@ -41,7 +37,6 @@ void represent_gauge_field_gpu() {
         const int grid = (N - 1) / BLOCK_SIZE + 1;
         represent_gauge_field_kernel<<<grid, BLOCK_SIZE>>>(u_gauge_f->gpu_ptr, gauge_ptr, N, block_start);
     }
-}
 }
 
 #endif
