8b64c98cd9167fe4287577e72052608db9cb4893
Antonio Rago <antonio.rago@plymouth.ac.uk>
2023-06-20 13:16:29 +0200

    cosmetic change for compatibility with GPU version

--- LibHR/Utils/clover_exp.c
+++ LibHR/Utils/clover_exp.c
@@ -474,7 +474,7 @@ static void doublehornerNF3(double *C, suNfc *A)
       q[2 * NF - 1] = q[2 * NF - 2];
       for (k = 2 * NF - 2; k > 0; k--)
         q[k] = q[k - 1] - creal(p[k]) * qlast;
-      q[0] = -p[0] * qlast + q2[j][i];
+      q[0] = -creal(p[0]) * qlast + q2[j][i];
     }
 
     for (j = 0; j < 2 * NF; j++)
@@ -545,7 +545,7 @@ static void doublehornerNF2(double *C, suNfc *A)
       q[2 * NF - 1] = q[2 * NF - 2];
       for (k = 2 * NF - 2; k > 0; k--)
         q[k] = q[k - 1] - creal(p[k]) * qlast;
-      q[0] = -p[0] * qlast + q2[j][i];
+      q[0] = -creal(p[0]) * qlast + q2[j][i];
     }
 
     for (j = 0; j < 2 * NF; j++)
