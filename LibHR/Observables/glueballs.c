a1af1bdbffdb6d18130e26c90d229f5c24311992
Antonio Rago <antonio.rago@plymouth.ac.uk>
2024-05-02 16:02:54 +0200

    Error in the reset of the gluebals smear fields

--- LibHR/Observables/glueballs.c
+++ LibHR/Observables/glueballs.c
@@ -7,17 +7,15 @@
 #include "glueballs.h"
 #include "global.h"
 
-void measure_1pt_glueballs(int nblockingstart, int nblockingend, double *smear_val, hr_complex *gb_storage)
-{
+void measure_1pt_glueballs(int nblockingstart, int nblockingend, double *smear_val, hr_complex *gb_storage) {
     int i, nt;
     int wrk1, wrk2 = -1;
     hr_complex *point_gb;
+    reset_wrk_pointers();
 
     point_gb = gb_storage;
 
-    for (i = 0; i < nblockingstart; i++)
-    {
-
+    for (i = 0; i < nblockingstart; i++) {
         wrk1 = spatial_APE_smear_wrkspace(smear_val, wrk2);
 
         release_wrk_space(wrk2);
@@ -27,11 +25,9 @@ void measure_1pt_glueballs(int nblockingstart, int nblockingend, double *smear_v
         release_wrk_space(wrk1);
     }
 
-    for (i = 0; i < nblockingend - nblockingstart; i++)
-    {
+    for (i = 0; i < nblockingend - nblockingstart; i++) {
         point_gb = gb_storage + i * total_n_glue_op;
-        for (nt = 0; nt < n_active_slices; nt++)
-        {
+        for (nt = 0; nt < n_active_slices; nt++) {
             eval_all_glueball_ops(active_slices_list[nt], point_gb);
             point_gb += total_n_glue_op * (nblockingend - nblockingstart + 1);
         }
@@ -47,8 +43,7 @@ void measure_1pt_glueballs(int nblockingstart, int nblockingend, double *smear_v
 
     point_gb = gb_storage + (nblockingend - nblockingstart) * total_n_glue_op;
 
-    for (nt = 0; nt < n_active_slices; nt++)
-    {
+    for (nt = 0; nt < n_active_slices; nt++) {
         eval_all_glueball_ops(active_slices_list[nt], point_gb);
         point_gb += total_n_glue_op * (nblockingend - nblockingstart + 1);
     }
@@ -56,8 +51,7 @@ void measure_1pt_glueballs(int nblockingstart, int nblockingend, double *smear_v
     release_wrk_space(wrk2);
 }
 
-void measure_1pt_torellons(double *smear_val, hr_complex *tor_storage, hr_complex **pf)
-{
+void measure_1pt_torellons(double *smear_val, hr_complex *tor_storage, hr_complex **pf) {
     int nt;
     int wrk1;
     hr_complex *point_tor;
@@ -66,8 +60,7 @@ void measure_1pt_torellons(double *smear_val, hr_complex *tor_storage, hr_comple
 
     wrk1 = spatial_APE_smear_wrkspace(smear_val, -1);
 
-    for (nt = 0; nt < n_active_slices; nt++)
-    {
+    for (nt = 0; nt < n_active_slices; nt++) {
         eval_all_torellon_ops(active_slices_list[nt], point_tor, pf);
         point_tor += total_n_tor_op;
     }
@@ -75,9 +68,7 @@ void measure_1pt_torellons(double *smear_val, hr_complex *tor_storage, hr_comple
     release_wrk_space(wrk1);
 }
 
-wilson_lines *polyleg(int ix, int d)
-{
-
+wilson_lines *polyleg(int ix, int d) {
     int in = ix, in1 = 0;
     suNg *w1, *w2, *w3, s1, s2;
 
@@ -86,8 +77,7 @@ wilson_lines *polyleg(int ix, int d)
     static wilson_lines *poly = NULL;
     static int *LL;
 
-    if (poly == NULL)
-    {
+    if (poly == NULL) {
         poly = (wilson_lines *)malloc(3 * sizeof(wilson_lines));
         poly[0].p = (suNg *)malloc(npoly_dist * sizeof(suNg));
         poly[1].p = (suNg *)malloc(npoly_dist * sizeof(suNg));
@@ -100,20 +90,17 @@ wilson_lines *polyleg(int ix, int d)
         LL[3] = Z;
     }
 
-    if (ix == poly[d - 1].ix)
-        return poly + d - 1;
+    if (ix == poly[d - 1].ix) { return poly + d - 1; }
 
-    if (idn_wrk(ix, d) != poly[d - 1].ix)
-    {
-        for (int i = 0; i < npoly_dist; i++)
+    if (idn_wrk(ix, d) != poly[d - 1].ix) {
+        for (int i = 0; i < npoly_dist; i++) {
             in = iup_wrk(in, d);
+        }
 
         in1 = in;
         _suNg_unit(*w2);
 
-        for (int i = npoly_dist; i < LL[d] - 1; i++)
-        {
-
+        for (int i = npoly_dist; i < LL[d] - 1; i++) {
             w1 = pu_gauge_wrk(in, d);
             _suNg_times_suNg(*w3, *w2, *w1);
             w1 = w2;
@@ -122,19 +109,15 @@ wilson_lines *polyleg(int ix, int d)
             in = iup_wrk(in, d);
         }
 
-        if (npoly_dist < LL[d])
-        {
+        if (npoly_dist < LL[d]) {
             w1 = pu_gauge_wrk(in, d);
             _suNg_times_suNg(poly[d - 1].p[npoly_dist - 1], *w2, *w1);
-        }
-        else
-        {
+        } else {
             _suNg_unit(poly[d - 1].p[npoly_dist - 1]);
         }
 
         in = in1;
-        for (int i = npoly_dist - 2; i >= 0; i--)
-        {
+        for (int i = npoly_dist - 2; i >= 0; i--) {
             in = idn_wrk(in, d);
             w1 = pu_gauge_wrk(in, d);
             _suNg_times_suNg(poly[d - 1].p[i], *w1, poly[d - 1].p[i + 1]);
@@ -144,9 +127,7 @@ wilson_lines *polyleg(int ix, int d)
         w1 = pu_gauge_wrk(in, d);
         _suNg_times_suNg(*w3, *w1, poly[d - 1].p[0]);
         _suNg_trace(poly[d - 1].tr, *w3);
-    }
-    else
-    {
+    } else {
         in1 = idn_wrk(ix, d);
         in = ix;
 
@@ -156,8 +137,7 @@ wilson_lines *polyleg(int ix, int d)
         _suNg_times_suNg(s2, poly[d - 1].p[0], *w1);
         _suNg_dagger_times_suNg(poly[d - 1].p[0], *w2, s2);
 
-        for (int i = 1; i < npoly_dist; i++)
-        {
+        for (int i = 1; i < npoly_dist; i++) {
             in = iup_wrk(in, d);
             w2 = pu_gauge_wrk(in, d);
             _suNg_dagger_times_suNg(poly[d - 1].p[i], *w2, poly[d - 1].p[i - 1]);
